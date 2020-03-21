8f1398b0121578f18d8ad235144dc4445c300db4
8infy <99danilt@gmail.com>
2020-03-21 16:41:45 +0300

    [Scripts] ISO now builds in a separate folder.
    - This prevents an issue where the ISO image contained a lot of garbage.

--- Boot/build_iso.sh
+++ Boot/build_iso.sh
@@ -2,10 +2,14 @@ echo "Building UltraOS iso image..."
 MYDIR="$(dirname "$(realpath "$0")")"
 
 /bin/bash $MYDIR/build_floppy.sh
+mkdir $MYDIR/iso
+cp $MYDIR/UltraFloppy.flp $MYDIR/iso/
 genisoimage -V 'UltraVolume' \
             -input-charset iso8859-1 \
             -o $MYDIR/UltraDisk.iso \
             -b UltraFloppy.flp \
-            -hide UltraFloppy.flp $MYDIR
+            -hide UltraFloppy.flp $MYDIR/iso
+rm $MYDIR/iso/UltraFloppy.flp
+rmdir $MYDIR/iso
 echo "Done!"
 
