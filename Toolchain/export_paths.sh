05092352ce7738a6a3f396c46966a56c872b5dde
8infy <99danilt@gmail.com>
2020-04-22 20:17:43 +0300

    [Scripts] export_paths.sh now exports correct paths from any location.

--- Toolchain/export_paths.sh
+++ Toolchain/export_paths.sh
@@ -1,2 +1,2 @@
-true_path="$(realpath "$(dirname "${BASH_SOURCE[${#BASH_SOURCE[@]} - 1]}")")"
-export PATH="$true_path/CrossCompiler/Tools/bin:$PATH"
+true_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PATH="$true_path/CrossCompiler/Tools/bin:$PATH"
