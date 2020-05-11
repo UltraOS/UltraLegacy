39e0511ef650dcd0af3a2d8cd9bf8263c09ea601
8infy <99danilt@gmail.com>
2020-05-11 15:29:22 +0300

    [Boot] Rewrote all boot-related parts to run from a FAT32 hard disk.
    There's a lot of code duplication, hardcoded values/offsets, barely
    any error checking, so all of this is to come later on. This is just
    the first working prototype, so don't expect much of it in terms of
    good code.

--- Boot/UKLoader.asm
+++ Boot/UKLoader.asm
@@ -14,22 +14,70 @@ start:
     push ds
     xor ax, ax
     mov ds, ax
-    mov si, BPB_OFFSET
+    mov si, VBR_ORIGIN + BPB_OFFSET
     mov di, BPB
     mov cx, BPB_SIZE
     rep movsb
     pop ds
 
-    ; find the kernel file cluster
-    find_file_on_disk kernel_file, KERNEL_SEGMENT
-    mov [kernel_file_cluster], ax
+    ; calculate the offset to data in sectors
+    mov eax, [SectorsPerFAT]
+    xor ebx, ebx
+    mov bx,  [FATCount]
+    mul ebx
+    add ax,  [SectorsReserved]
+    add eax, [SectorsHidden]
+    mov [data_offset_in_sectors], eax
 
-    ; read the kernel file into its segment
-    read_file_from_disk [kernel_file_cluster], KERNEL_SEGMENT
+    ; calculate offset to FAT
+    mov eax, [SectorsHidden]
+    add ax,  [SectorsReserved]
+    mov [fat_offset_in_sectors], eax
 
-    ; reset the disk system
-    mov dl, [BootDrive]
-    call reset_disk_system
+    ; read the first sector of the root directory
+    mov dl, [boot_drive]
+    mov ax, KERNEL_SEGMENT
+    mov es, ax
+    xor di, di
+    mov eax, [RootDirCluster]
+    sub eax, RESERVED_CLUSTER_COUNT
+    add eax, [data_offset_in_sectors]
+    call read_disk_lba
+
+    ; check if this file is what we're looking for
+    mov cx, FAT_FILENAME_LENGTH
+    mov ax, KERNEL_SEGMENT
+    mov es, ax
+    mov ax, 32
+    mov di, ax
+    mov si, kernel_file
+    repz cmpsb
+    jne on_file_not_found
+
+    on_file_found:
+        mov eax, [es: 32 + FILESIZE_ENTRY_OFFSET]
+        xor ebx, ebx
+        xor edx, edx
+        mov bx, [BytesPerSector]
+        div ebx
+        cmp edx, 0
+        je aligned ; fix this hack :D
+        add eax, 1
+        aligned:
+        mov [DAP.sector_count], ax
+
+        mov ax, [es:32 + CLUSTER_LOW_ENTRY_OFFSET]
+        sub eax, RESERVED_CLUSTER_COUNT
+        add eax, [data_offset_in_sectors]
+
+        push eax
+        mov ax, KERNEL_SEGMENT
+        mov es, ax
+        xor di, di
+        pop eax
+        mov dl, [boot_drive]
+
+        call read_disk_lba
 
     mov si, loading_msg
     call write_string
@@ -137,6 +185,11 @@ start:
     dd KERNEL_FLAT_ADDRESS
     dw 0x0008
 
+    on_file_not_found:
+        mov si, no_file_error
+        call write_string
+        call reboot
+
 %include "Common.inc"
 %include "LoaderUtils.inc"
 
@@ -148,14 +201,32 @@ gdt_entry:
     dw 24
     dd 2048
 
-BPB_OFFSET: equ 0x7C03
+VBR_ORIGIN: equ 0x7C00
+BPB_OFFSET: equ 11
 KERNEL_SEGMENT: equ 0x2000
 KERNEL_FLAT_ADDRESS: equ 0x20000 ; KERNEL_SEGMENT << 4
 BPB_SIZE: equ 34
 
-dskread_msg db "Reading kernel from disk...", CR, LF, 0
-loading_msg db "Preparing kernel environment...", CR, LF, 0
-a20fail_msg db "Failed to enable A20!", CR, LF, 0
+CLUSTER_HIGH_ENTRY_OFFSET: equ 20
+CLUSTER_LOW_ENTRY_OFFSET:  equ 26
+FILESIZE_ENTRY_OFFSET:     equ 28
+RESERVED_CLUSTER_COUNT:    equ 2
+KERNEL_LOADER_SEGMENT:     equ 0x1000
+FAT_SEGMENT:               equ 0x2000
+FAT_FILENAME_LENGTH:       equ 11
+ENTRIES_PER_FAT_SECTOR:    equ 128
+END_OF_CHAIN:              equ 0x0FFFFFF8
+KERNEL_LOADER_CLUSTER:     equ 3
+
+boot_drive:             db 0x80
+this_partition:         dw 0x0
+data_offset_in_sectors: dd 0x0
+fat_offset_in_sectors:  dd 0x0
+
+dskread_msg:        db "Reading kernel from disk...", CR, LF, 0
+loading_msg:        db "Preparing kernel environment...", CR, LF, 0
+a20fail_msg:        db "Failed to enable A20!", CR, LF, 0
+no_file_error:      db "Couldn't find the kernel file!", CR, LF, 0
 
 kernel_file db "Kernel  bin"
 kernel_file_cluster dw 0
--- Boot/UltraBoot.asm
+++ Boot/UltraBoot.asm
@@ -5,16 +5,15 @@ main:
     jmp short start
     nop
 
+OEMLabel db "UltraOS "
 %include "BPB.inc"
 %include "CommonMacros.inc"
 
 start:
-    ; initilize memory segments:
-    ; segment 0 
-    ; offset  0x7C00
-    ; stack   0x7C00
+    ; initilize memory segments
     cli
-    mov [BootDrive], dl
+    mov [boot_drive], dl
+    mov [this_partition], bx
     xor ax, ax
     mov ds, ax
     mov es, ax
@@ -22,39 +21,97 @@ start:
     mov sp, 0x7C00
     sti
 
-    clear_screen
+    ; calculate the offset to data in sectors
+    mov eax, [SectorsPerFAT]
+    xor ebx, ebx
+    mov bx,  [FATCount]
+    mul ebx
+    add ax,  [SectorsReserved]
+    add eax, [SectorsHidden]
+    mov [data_offset_in_sectors], eax
 
-    mov si, loading_msg
-    call write_string
+    ; calculate offset to FAT
+    mov eax, [SectorsHidden]
+    add ax,  [SectorsReserved]
+    mov [fat_offset_in_sectors], eax
 
-    mov dl, [BootDrive]
-    call reset_disk_system
-    jc on_boot_failed
-
-    ; read the kernel loader from disk
-    find_file_on_disk kernel_loader_file, KERNEL_LOADER_SEGMENT
-    mov [kernel_loader_cluster], ax
-
-    load_fat
-
-    read_file_from_disk [kernel_loader_cluster], KERNEL_LOADER_SEGMENT
-
-    ; jump to kernel loader
-    ; it expects the segments to be initilized
-    ; according to its origin so we set them here
+    ; read the first sector of the root directory
+    mov dl, [boot_drive]
     mov ax, KERNEL_LOADER_SEGMENT
     mov es, ax
-    mov ds, ax
-    jmp KERNEL_LOADER_SEGMENT:0x0
+    xor di, di
+    mov eax, [RootDirCluster]
+    sub eax, RESERVED_CLUSTER_COUNT
+    add eax, [data_offset_in_sectors]
+    call read_disk_lba
+
+    ; check if this file is what we're looking for
+    mov cx, FAT_FILENAME_LENGTH
+    mov ax, KERNEL_LOADER_SEGMENT
+    mov es, ax
+    xor di, di
+    mov si, kernel_loader_file
+    repz cmpsb
+    jne on_file_not_found
+
+    on_file_found:
+        mov eax, [es:FILESIZE_ENTRY_OFFSET]
+        xor ebx, ebx
+        xor edx, edx
+        mov bx, [BytesPerSector]
+        div ebx
+        cmp edx, 0
+        je aligned ; fix this hack :D
+        add eax, 1
+        aligned:
+        mov [DAP.sector_count], ax
+
+        mov ax, [es:CLUSTER_LOW_ENTRY_OFFSET]
+        sub eax, RESERVED_CLUSTER_COUNT
+        add eax, [data_offset_in_sectors]
+
+        push eax
+        mov ax, KERNEL_LOADER_SEGMENT
+        mov es, ax
+        xor di, di
+        pop eax
+        mov dl, [boot_drive]
+
+        call read_disk_lba
+
+    transfer_control_to_kernel_loader:
+        mov ax, KERNEL_LOADER_SEGMENT
+        mov ds, ax
+        mov es, ax
+        mov ax, boot_context
+        jmp KERNEL_LOADER_SEGMENT:0x0
+
+    on_file_not_found:
+        mov si, no_file_error
+        call write_string
+        call reboot
 
 %include "Common.inc"
 
-KERNEL_LOADER_SEGMENT: equ 0x1000
+CLUSTER_HIGH_ENTRY_OFFSET: equ 20
+CLUSTER_LOW_ENTRY_OFFSET:  equ 26
+FILESIZE_ENTRY_OFFSET:     equ 28
+RESERVED_CLUSTER_COUNT:    equ 2
+KERNEL_LOADER_SEGMENT:     equ 0x1000
+FAT_SEGMENT:               equ 0x2000
+FAT_FILENAME_LENGTH:       equ 11
+ENTRIES_PER_FAT_SECTOR:    equ 128
+END_OF_CHAIN:              equ 0x0FFFFFF8
+KERNEL_LOADER_CLUSTER:     equ 3
 
-kernel_loader_cluster dw 0
-kernel_loader_file    db "UKLoaderbin"
+kernel_loader_file:     db "UKLoaderbin"
+no_file_error:          db "Couldn't find the kernel loader file!", CR, LF, 0
 
-loading_msg db "Loading UltraOS...", CR, LF, 0x0
+boot_context:
+    boot_drive:             db 0
+    this_partition:         dw 0x0
+    data_offset_in_sectors: dd 0x0
+    fat_offset_in_sectors:  dd 0x0
 
 times 510-($-$$) db 0
 boot_signature   dw 0xAA55
