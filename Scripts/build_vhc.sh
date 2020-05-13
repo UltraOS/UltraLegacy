e6f35a7d165e80684e859ccce2cbd93aafb6ccc5
8infy <99danilt@gmail.com>
2020-05-13 19:53:49 +0300

    [Scripts] VHC is now compiled in release by default.

--- Scripts/build_vhc.sh
+++ Scripts/build_vhc.sh
@@ -5,6 +5,7 @@ true_path="$(dirname "$(realpath "$0")")"
 on_error()
 {
     echo "VHC build failed!"
+    rm -rf vhc-src
     exit 1
 }
 
@@ -30,7 +31,7 @@ git clone https://github.com/UltraOS/VHC vhc-src || on_error
 pushd vhc-src
 mkdir build || on_error
 pushd build
-cmake .. && cmake --build . || on_error
+cmake .. -DCMAKE_BUILD_TYPE=Release && cmake --build . || on_error
 mv ./vhc $true_path || on_error
 popd
 popd
