a19af731977a04744afa572f500c26ef02ef4c97
8infy <99danilt@gmail.com>
2020-05-24 15:27:20 +0300

    [Kernel] Added a few new macros.

--- Kernel/Macros.h
+++ Kernel/Macros.h
@@ -2,4 +2,11 @@
 
 #define PACKED [[gnu::packed]]
 
+#define USED __attribute__((used))
+
 #define SET_BIT(x) (1 << (x))
+
+#define TO_STRING(x) #x
+
+#define cli() asm volatile("cli" ::: "memory")
+#define sti() asm volatile("sti" ::: "memory")
