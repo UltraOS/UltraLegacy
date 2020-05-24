52e950c220b9c60e74371b00a1a97dd2159539ee
8infy <99danilt@gmail.com>
2020-05-24 15:27:59 +0300

    [Kernel] Added InterruptDescriptorTable.
    Yay! Finally getting to interrupts.

---
+++ Kernel/InterruptDescriptorTable.cpp
@@ -0,0 +1,42 @@
+#include "InterruptDescriptorTable.h"
+#include "Log.h"
+namespace kernel {
+    InterruptDescriptorTable InterruptDescriptorTable::s_instance;
+
+    InterruptDescriptorTable::InterruptDescriptorTable()
+        : m_entries(), m_pointer{entry_count * sizeof(entry) - 1, m_entries}
+    {
+    }
+
+    InterruptDescriptorTable& InterruptDescriptorTable::the()
+    {
+        return s_instance;
+    }
+
+    InterruptDescriptorTable& InterruptDescriptorTable::register_isr(u16 index, attributes attrs, isr handler)
+    {
+        auto& the_entry = m_entries[index];
+
+        the_entry.address_lower  =  reinterpret_cast<u32>(handler) & 0x0000FFFF;
+        the_entry.address_higher = (reinterpret_cast<u32>(handler) & 0xFFFF0000) >> 16;
+        the_entry.attributes = attrs;
+        the_entry.selector = gdt_selector;
+
+        return *this;
+    }
+
+    InterruptDescriptorTable& InterruptDescriptorTable::register_interrupt_handler(u16 index, isr handler)
+    {
+        return register_isr(index, attributes(INTERRUPT_GATE | RING_3 | PRESENT), handler);
+    }
+
+    InterruptDescriptorTable& InterruptDescriptorTable::register_user_interrupt_handler(u16 index, isr handler)
+    {
+        return register_isr(index, attributes(TRAP_GATE | RING_3 | PRESENT), handler);
+    }
+
+    void InterruptDescriptorTable::install()
+    {
+        asm("lidt %0" ::"m"(m_pointer));
+    }
+}
---
+++ Kernel/InterruptDescriptorTable.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "Types.h"
+#include "Macros.h"
+
+namespace kernel {
+    class InterruptDescriptorTable {
+    public:
+        enum attributes : u8
+        {
+            RING_0         = 0b0,
+            RING_3         = SET_BIT(5) | SET_BIT(6),
+            TASK_GATE      = SET_BIT(0) | SET_BIT(2),
+            INTERRUPT_GATE = SET_BIT(1) | SET_BIT(2) | SET_BIT(3),
+            TRAP_GATE      = SET_BIT(0) | SET_BIT(1) | SET_BIT(2) | SET_BIT(3),
+            PRESENT        = SET_BIT(7),
+        };
+
+        static constexpr u16 entry_count = 256;
+        using isr = void(*)();
+
+        InterruptDescriptorTable& register_isr(u16 index, attributes attrs, isr handler);
+        InterruptDescriptorTable& register_interrupt_handler(u16 index, isr handler);
+        InterruptDescriptorTable& register_user_interrupt_handler(u16 index, isr handler);
+
+        void install();
+
+        static InterruptDescriptorTable& the();
+    private:
+        static constexpr u16 gdt_selector = 0x8;
+
+        InterruptDescriptorTable();
+
+        struct PACKED entry
+        {
+            u16 address_lower;
+            u16 selector;
+            u8  unused;
+            u8  attributes;
+            u16 address_higher;
+        } m_entries[entry_count];
+
+        struct PACKED pointer
+        {
+            u16     size;
+            entry*  address;
+        } m_pointer;
+
+        static InterruptDescriptorTable s_instance;
+    };
+}
