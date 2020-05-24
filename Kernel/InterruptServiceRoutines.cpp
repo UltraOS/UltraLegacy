0dc4d88730dbca4b194b2579b5d0f3dcaf5bd0e1
8infy <99danilt@gmail.com>
2020-05-24 15:29:12 +0300

    [Kernel] Added InterruptServiceRoutines.
    Now handling CPU exceptions! :)

---
+++ Kernel/InterruptServiceRoutines.cpp
@@ -0,0 +1,242 @@
+#include "InterruptDescriptorTable.h"
+#include "InterruptServiceRoutines.h"
+#include "Macros.h"
+#include "Log.h"
+
+namespace kernel {
+    DEFINE_INTERRUPT_HANDLER_NO_ERROR_CODE(division_by_zero, 24)
+    void InterruptServiceRoutines::division_by_zero_handler(RegisterState registers)
+    {
+        (void) registers;
+
+        log() << "Division by zero!";
+
+        for(;;);
+    }
+
+    DEFINE_INTERRUPT_HANDLER_NO_ERROR_CODE(debug, 13)
+    void InterruptServiceRoutines::debug_handler(RegisterState registers)
+    {
+        (void) registers;
+
+        log() << "Debug handler!";
+
+        for(;;);
+    }
+
+    DEFINE_INTERRUPT_HANDLER_NO_ERROR_CODE(non_maskable, 20)
+    void InterruptServiceRoutines::non_maskable_handler(RegisterState registers)
+    {
+        (void) registers;
+        
+        log() << "Non maskable interrupt!";
+
+        for(;;);
+    }
+
+    DEFINE_INTERRUPT_HANDLER_NO_ERROR_CODE(breakpoint, 18)
+    void InterruptServiceRoutines::breakpoint_handler(RegisterState registers)
+    {
+        (void) registers;
+        
+        log() << "Breakpoint interrupt!";
+
+        for(;;);
+    }
+
+    DEFINE_INTERRUPT_HANDLER_NO_ERROR_CODE(overflow, 16)
+    void InterruptServiceRoutines::overflow_handler(RegisterState registers)
+    {
+        (void) registers;
+
+        log() << "Overflow interrupt!";
+
+        for(;;);
+    }
+
+    DEFINE_INTERRUPT_HANDLER_NO_ERROR_CODE(bound_range_exceeded, 28)
+    void InterruptServiceRoutines::bound_range_exceeded_handler(RegisterState registers)
+    {
+        (void) registers;
+
+        log() << "Bound range exceeded!";
+
+        for(;;);
+    }
+
+    DEFINE_INTERRUPT_HANDLER_NO_ERROR_CODE(invalid_opcode, 22)
+    void InterruptServiceRoutines::invalid_opcode_handler(RegisterState registers)
+    {
+        (void) registers;
+
+        log() << "Invalid OPcode!";
+
+        for(;;);
+    }
+
+    DEFINE_INTERRUPT_HANDLER_NO_ERROR_CODE(device_not_available, 28)
+    void InterruptServiceRoutines::device_not_available_handler(RegisterState registers)
+    {
+        (void) registers;
+
+        log() << "Device not available!";
+
+        for(;;);
+    }
+
+    DEFINE_INTERRUPT_HANDLER(double_fault, 20)
+    void InterruptServiceRoutines::double_fault_handler(RegisterState registers)
+    {
+        (void) registers;
+
+        log() << "Double fault!";
+
+        for(;;);
+    }
+
+    DEFINE_INTERRUPT_HANDLER_NO_ERROR_CODE(coprocessor_segment_overrun, 35)
+    void InterruptServiceRoutines::coprocessor_segment_overrun_handler(RegisterState registers)
+    {
+        (void) registers;
+
+        log() << "Coprocessor segment overrun! (really?)";
+
+        for(;;);
+    }
+
+    DEFINE_INTERRUPT_HANDLER(invalid_tss, 19)
+    void InterruptServiceRoutines::invalid_tss_handler(RegisterState registers)
+    {
+        (void) registers;
+
+        log() << "Invalid TSS!";
+
+        for(;;);
+    }
+
+    DEFINE_INTERRUPT_HANDLER(segment_not_present, 27)
+    void InterruptServiceRoutines::segment_not_present_handler(RegisterState registers)
+    {
+        (void) registers;
+
+        log() << "Segment not present!";
+
+        for(;;);
+    }
+
+    DEFINE_INTERRUPT_HANDLER(stack_segment_fault, 27)
+    void InterruptServiceRoutines::stack_segment_fault_handler(RegisterState registers)
+    {
+        (void) registers;
+
+        log() << "Stack segment fault!";
+
+        for(;;);
+    }
+
+    DEFINE_INTERRUPT_HANDLER(general_protection_fault, 32)
+    void InterruptServiceRoutines::general_protection_fault_handler(RegisterState registers)
+    {
+        (void) registers;
+
+        log() << "General protection fault!";
+
+        for(;;);
+    }
+
+    DEFINE_INTERRUPT_HANDLER(pagefault, 17)
+    void InterruptServiceRoutines::pagefault_handler(RegisterState registers)
+    {
+        (void) registers;
+
+        log() << "Pagefault!";
+
+        for(;;);
+    }
+
+    DEFINE_INTERRUPT_HANDLER_NO_ERROR_CODE(floating_point_exception, 32)
+    void InterruptServiceRoutines::floating_point_exception_handler(RegisterState registers)
+    {
+        (void) registers;
+
+        log() << "Floating point exception!";
+
+        for(;;);
+    }
+
+    DEFINE_INTERRUPT_HANDLER(alignment_check_exception, 33)
+    void InterruptServiceRoutines::alignment_check_exception_handler(RegisterState registers)
+    {
+        (void) registers;
+
+        log() << "Alignment Check exception!";
+
+        for(;;);
+    }
+
+    DEFINE_INTERRUPT_HANDLER_NO_ERROR_CODE(machine_check_exception, 31)
+    void InterruptServiceRoutines::machine_check_exception_handler(RegisterState registers)
+    {
+        (void) registers;
+
+        log() << "Machine check exception!";
+
+        for(;;);
+    }
+
+    DEFINE_INTERRUPT_HANDLER_NO_ERROR_CODE(simd_floating_point_exception, 37)
+    void InterruptServiceRoutines::simd_floating_point_exception_handler(RegisterState registers)
+    {
+        (void) registers;
+
+        log() << "SIMD floating point exception!";
+
+        for(;;);
+    }
+
+    DEFINE_INTERRUPT_HANDLER_NO_ERROR_CODE(virtualization_exception, 32)
+    void InterruptServiceRoutines::virtualization_exception_handler(RegisterState registers)
+    {
+        (void) registers;
+
+        log() << "Virtualization exception!";
+
+        for(;;);
+    }
+
+    DEFINE_INTERRUPT_HANDLER(security_exception, 26)
+    void InterruptServiceRoutines::security_exception_handler(RegisterState registers)
+    {
+        (void) registers;
+
+        log() << "Security exception!";
+
+        for(;;);
+    }
+
+    void InterruptServiceRoutines::install()
+    {
+        InterruptDescriptorTable::the()
+            .register_interrupt_handler(division_by_zero_index, division_by_zero_entry)
+            .register_user_interrupt_handler(debug_index, debug_entry)
+            .register_interrupt_handler(non_maskable_index, non_maskable_entry)
+            .register_user_interrupt_handler(breakpoint_index, breakpoint_entry)
+            .register_user_interrupt_handler(overflow_index, overflow_entry)
+            .register_interrupt_handler(bound_range_exceeded_index, bound_range_exceeded_entry)
+            .register_interrupt_handler(invalid_opcode_index, invalid_opcode_entry)
+            .register_interrupt_handler(device_not_available_index, device_not_available_entry)
+            .register_interrupt_handler(double_fault_index, double_fault_entry)
+            .register_interrupt_handler(coprocessor_segment_overrun_index, coprocessor_segment_overrun_entry)
+            .register_interrupt_handler(invalid_tss_index, invalid_opcode_entry)
+            .register_interrupt_handler(segment_not_present_index, segment_not_present_entry)
+            .register_interrupt_handler(stack_segment_fault_index, stack_segment_fault_entry)
+            .register_interrupt_handler(general_protection_fault_index, general_protection_fault_entry)
+            .register_interrupt_handler(pagefault_index, pagefault_entry)
+            .register_interrupt_handler(floating_point_exception_index, floating_point_exception_entry)
+            .register_interrupt_handler(alignment_check_exception_index, alignment_check_exception_entry)
+            .register_interrupt_handler(machine_check_exception_index, machine_check_exception_entry)
+            .register_interrupt_handler(simd_floating_point_exception_index, simd_floating_point_exception_entry)
+            .register_interrupt_handler(virtualization_exception_index, virtualization_exception_entry)
+            .register_interrupt_handler(security_exception_index, security_exception_entry);
+    }
+}
---
+++ Kernel/InterruptServiceRoutines.h
@@ -0,0 +1,142 @@
+#pragma once
+
+#include "Types.h"
+#include "Macros.h"
+
+namespace kernel {
+    struct PACKED RegisterState
+    {
+        u32 ss;
+        u32 gs;
+        u32 fs;
+        u32 es;
+        u32 ds;
+        u32 edi;
+        u32 esi;
+        u32 ebp;
+        u32 esp;
+        u32 ebx;
+        u32 edx;
+        u32 ecx;
+        u32 eax;
+        u32 error_code;
+        u32 eip;
+        u32 cs;
+        u32 eflags;
+        u32 userspace_esp;
+        u32 userspace_ss;
+    };
+
+    class InterruptServiceRoutines
+    {
+    private:
+        static constexpr u16 division_by_zero_index              = 0;
+        static constexpr u16 debug_index                         = 1;
+        static constexpr u16 non_maskable_index                  = 2;
+        static constexpr u16 breakpoint_index                    = 3;
+        static constexpr u16 overflow_index                      = 4;
+        static constexpr u16 bound_range_exceeded_index          = 5;
+        static constexpr u16 invalid_opcode_index                = 6;
+        static constexpr u16 device_not_available_index          = 7;
+        static constexpr u16 double_fault_index                  = 8;
+        static constexpr u16 coprocessor_segment_overrun_index   = 9;
+        static constexpr u16 invalid_tss_index                   = 10;
+        static constexpr u16 segment_not_present_index           = 11;
+        static constexpr u16 stack_segment_fault_index           = 12;
+        static constexpr u16 general_protection_fault_index      = 13;
+        static constexpr u16 pagefault_index                     = 14;
+        static constexpr u16 floating_point_exception_index      = 16;
+        static constexpr u16 alignment_check_exception_index     = 17;
+        static constexpr u16 machine_check_exception_index       = 18;
+        static constexpr u16 simd_floating_point_exception_index = 19;
+        static constexpr u16 virtualization_exception_index      = 20;
+        static constexpr u16 security_exception_index            = 30;
+    private:
+        static void division_by_zero_handler(RegisterState)              USED;
+        static void debug_handler(RegisterState)                         USED;
+        static void non_maskable_handler(RegisterState)                  USED;
+        static void breakpoint_handler(RegisterState)                    USED;
+        static void overflow_handler(RegisterState);                     USED;
+        static void bound_range_exceeded_handler(RegisterState)          USED;
+        static void invalid_opcode_handler(RegisterState)                USED;
+        static void device_not_available_handler(RegisterState)          USED;
+        static void double_fault_handler(RegisterState)                  USED;
+        static void coprocessor_segment_overrun_handler(RegisterState)   USED;
+        static void invalid_tss_handler(RegisterState)                   USED;
+        static void segment_not_present_handler(RegisterState)           USED;
+        static void stack_segment_fault_handler(RegisterState)           USED;
+        static void general_protection_fault_handler(RegisterState)      USED;
+        static void pagefault_handler(RegisterState)                     USED;
+        static void floating_point_exception_handler(RegisterState)      USED;
+        static void alignment_check_exception_handler(RegisterState)     USED;
+        static void machine_check_exception_handler(RegisterState)       USED;
+        static void simd_floating_point_exception_handler(RegisterState) USED;
+        static void virtualization_exception_handler(RegisterState)      USED;
+        static void security_exception_handler(RegisterState)            USED;
+    public:
+        static void install();
+    };
+}
+
+// Super specific mangling rules going on here, be careful :)
+#define ISR(handler, len) "_ZN6kernel24InterruptServiceRoutines" \
+                          TO_STRING(len)                         \
+                          TO_STRING(handler)                     \
+                          "ENS_13RegisterStateE"
+
+// title - the interrupt handler title without "_handler"
+// length - the full length of the actual handler, including "_handler"
+// (and yes there isn't any other way to get the length as a literal AFAIK)
+#define DEFINE_INTERRUPT_HANDLER(title, length)         \
+    extern "C" void title##_entry();                    \
+    asm(                                                \
+        ".globl " #title "_entry\n"                     \
+        "" #title "_entry: \n"                          \
+        "    pusha\n"                                   \
+        "    pushl %ds\n"                               \
+        "    pushl %es\n"                               \
+        "    pushl %fs\n"                               \
+        "    pushl %gs\n"                               \
+        "    pushl %ss\n"                               \
+        "    mov $0x10, %ax\n"                          \
+        "    mov %ax, %ds\n"                            \
+        "    mov %ax, %es\n"                            \
+        "    cld\n"                                     \
+        "    call " ISR(title ## _handler, length) "\n" \
+        "    add $0x4, %esp \n"                         \
+        "    popl %gs\n"                                \
+        "    popl %fs\n"                                \
+        "    popl %es\n"                                \
+        "    popl %ds\n"                                \
+        "    popa\n"                                    \
+        "    add $0x4, %esp\n"                          \
+        "    iret\n");
+
+// title - the interrupt handler title without "_handler"
+// length - the full length of the actual handler, including "_handler"
+// (and yes there isn't any other way to get the length as a literal AFAIK)
+#define DEFINE_INTERRUPT_HANDLER_NO_ERROR_CODE(title, length)   \
+    extern "C" void title##_entry();                            \
+    asm(                                                        \
+        ".globl " #title "_entry\n"                             \
+        "" #title "_entry: \n"                                  \
+        "    pushl $0\n"                                        \
+        "    pusha\n"                                           \
+        "    pushl %ds\n"                                       \
+        "    pushl %es\n"                                       \
+        "    pushl %fs\n"                                       \
+        "    pushl %gs\n"                                       \
+        "    pushl %ss\n"                                       \
+        "    mov $0x10, %ax\n"                                  \
+        "    mov %ax, %ds\n"                                    \
+        "    mov %ax, %es\n"                                    \
+        "    cld\n"                                             \
+        "    call " ISR(title ## _handler, length) "\n"         \
+        "    add $0x4, %esp \n"                                 \
+        "    popl %gs\n"                                        \
+        "    popl %fs\n"                                        \
+        "    popl %es\n"                                        \
+        "    popl %ds\n"                                        \
+        "    popa\n"                                            \
+        "    add $0x4, %esp\n"                                  \
+        "    iret\n");
