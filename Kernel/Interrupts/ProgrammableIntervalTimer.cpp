fe4de6279f3fc1840e17d15096ad3bff167a7554
8infy <99danilt@gmail.com>
2020-05-31 12:44:25 +0300

    [Kernel/Time] Added a basic PIT implementation.

---
+++ Kernel/Interrupts/ProgrammableIntervalTimer.cpp
@@ -0,0 +1,72 @@
+#include "Core/Logger.h"
+#include "Core/IO.h"
+
+#include "ProgrammableIntervalTimer.h"
+
+namespace kernel {
+
+    ProgrammableIntervalTimer::ProgrammableIntervalTimer()
+        : InterruptRequestHandler(timer_irq)
+    {
+        set_frequency(default_ticks_per_second);
+    }
+
+    void ProgrammableIntervalTimer::set_frequency(u32 ticks_per_second)
+    {
+        if (ticks_per_second > timer_frequency)
+        {
+            error() << "Cannot set the timer to frequency " << ticks_per_second;
+            hang();
+        }
+
+        m_frequency = ticks_per_second;
+
+        u32 divisor = timer_frequency / ticks_per_second;
+
+        IO::out8<timer_command>(write_word | square_wave_mode | timer_0);
+
+        IO::out8<timer_data>(divisor & 0x000000FF);
+        IO::out8<timer_data>((divisor & 0x0000FF00) >> 8);
+    }
+
+    void ProgrammableIntervalTimer::on_irq(const RegisterState& registers)
+    {
+        static u32 tick = 0;
+        static u32 seconds_elapsed = 0;
+
+        ++tick;
+
+        auto display_write = [] (const char* string, bool carrige_return = false)
+                             {
+                                 static u16* memory = reinterpret_cast<u16*>(0xB8000);
+
+                                 if (carrige_return)
+                                     memory = reinterpret_cast<u16*>(0xB8000);
+
+                                 while (*string)
+                                 {
+                                     u16 colored_char = *(string++);
+                                     colored_char |= 0x2 << 8;
+
+                                     *(memory++) = colored_char;
+                                 }
+                             };
+
+        if (tick / m_frequency)
+        {
+            info() << "Timer: " << ++seconds_elapsed << " seconds uptime";
+            tick = 0;
+
+            display_write("Uptime: ", true);
+
+            bool ok;
+            char number[11];
+
+            to_string_buffer(seconds_elapsed, number, 11, ok);
+
+            display_write(number);
+
+            display_write(" seconds");
+        }
+    }
+}
---
+++ Kernel/Interrupts/ProgrammableIntervalTimer.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "InterruptRequestHandler.h"
+
+namespace kernel {
+
+    class ProgrammableIntervalTimer : public InterruptRequestHandler
+    {
+    public:
+        static constexpr u32 timer_frequency = 1193180;
+        static constexpr u32 default_ticks_per_second = 50;
+        static constexpr u8  timer_irq = 0;
+
+        static constexpr u8 timer_data       = 0x40;
+        static constexpr u8 timer_command    = 0x43;
+        static constexpr u8 timer_0          = 0x00;
+        static constexpr u8 square_wave_mode = 0x06;
+        static constexpr u8 write_word       = 0x30;
+
+        ProgrammableIntervalTimer();
+
+        void set_frequency(u32 ticks_per_second);
+
+        void on_irq(const RegisterState& registers) override;
+    private:
+        u32 m_frequency {0};
+    };
+}
