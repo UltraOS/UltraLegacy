aa4ef3fb52d6e99bca39a663d52360c1e3bff8f2
8infy <99danilt@gmail.com>
2020-06-13 14:02:29 +0300

    [Kernel/Common] Added more SFINAE helpers.

--- Kernel/Common/Traits.h
+++ Kernel/Common/Traits.h
@@ -18,9 +18,7 @@ struct integral_constant
     static constexpr T value = v;
 };
 
-
 // ---- remove_const_volatile ----
-
 template<typename T>
 struct remove_const_volatile
 {
@@ -47,12 +45,10 @@ struct remove_const_volatile<const volatile T>
 
 template<typename T>
 using remove_const_volatile_t = typename remove_const_volatile<T>::type;
-
 // --------------------
 
 // ---- is_integral ----
 // TODO: implement is_any_of and remove_const_volatile here
-
 template<typename T>
 struct is_integral : false_value {};
 
@@ -88,11 +84,9 @@ struct is_integral<unsigned long> : true_value {};
 
 template<typename T>
 inline constexpr bool is_integral_v = is_integral<T>::value;
-
 // --------------------
 
 // ---- is_floating_point ----
-
 template<typename T>
 struct is_floating_point : false_value {};
 
@@ -107,21 +101,17 @@ struct is_floating_point<long double> : true_value {};
 
 template<typename T>
 inline constexpr bool is_floating_point_v = is_floating_point<T>::value;
-
 // --------------------
 
 // ---- is_arithmetic ----
-
 template<typename T>
 struct is_arithmetic : integral_constant<bool, is_floating_point_v<T> || is_integral_v<T>> {};
 
 template<typename T>
 inline constexpr bool is_arithmetic_v = is_arithmetic<T>::value;
-
 // --------------------
 
 // ---- is_pointer ----
-
 template<typename T>
 struct is_actually_pointer : false_value {};
 
@@ -135,12 +125,56 @@ struct is_pointer : is_actually_pointer<typename remove_const_volatile<T>::type>
 
 template<typename T>
 inline constexpr bool is_pointer_v = is_pointer<T>::value;
-
 // --------------------
 
+// ---- is_trivially_constructible ----
+template <typename T>
+struct is_trivially_constructible : integral_constant<bool, __has_trivial_constructor(T)> {};
+
+template <typename T>
+inline constexpr bool is_trivially_constructible_v = is_trivially_constructible<T>::value;
+// --------------------
+
+// ---- is_trivially_destructible ----
+template <typename T>
+struct is_trivially_destructible : integral_constant<bool, __has_trivial_destructor(T)> {};
+
+template <typename T>
+inline constexpr bool is_trivially_destructible_v = is_trivially_destructible<T>::value;
+// --------------------
+
+// ---- is_trivially_copyable ----
+template <typename T>
+struct is_trivially_copyable : integral_constant<bool, __has_trivial_copy(T)> {};
+
+template <typename T>
+inline constexpr bool is_trivially_copyable_v = is_trivially_copyable<T>::value;
+// --------------------
+
+// ---- remove_reference ----
+template <typename T>
+struct remove_reference
+{
+    using type = T;
+};
+
+template <typename T>
+struct remove_reference<T&>
+{
+    using type = T;
+};
+
+template <typename T>
+struct remove_reference<T&&>
+{
+    using type = T;
+};
+
+template<typename T>
+using remove_reference_t = typename remove_reference<T>::type;
+// --------------------
 
 // ---- enable_if ----
-
 template<bool value, typename T = void>
 struct enable_if {};
 
@@ -152,5 +186,4 @@ struct enable_if<true, T>
 
 template <bool value, typename T = void>
 using enable_if_t = typename enable_if<value, T>::type;
-
 // --------------------
