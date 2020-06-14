9837092fc75324dcbb4c417bc847c391982ec758
8infy <99danilt@gmail.com>
2020-06-14 16:27:06 +0300

    [Kernel/Memory] Added Page.h.

---
+++ Kernel/Memory/Page.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Common/Types.h"
+
+namespace kernel {
+
+    class Page
+    {
+    public:
+        static constexpr size_t size = 4096;
+
+        Page(ptr_t physical_address)
+            : m_physical_address(physical_address)
+        {
+        }
+
+    private:
+        ptr_t m_physical_address;
+    };
+}
