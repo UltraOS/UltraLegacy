617ad07c96a06086dd1c5d5d0cf6c3ed16468696
8infy <99danilt@gmail.com>
2020-06-17 19:59:52 +0300

    [Kernel/Common] Added PageEntry.

---
+++ Kernel/Memory/PageEntry.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "Common/Types.h"
+
+namespace kernel {
+ 
+    class PageEntry
+    {
+    public:
+        void set_physical_address(ptr_t address)
+        {
+            m_physical_address = address;
+        }
+
+        ptr_t physical_address() const
+        {
+            return m_physical_address << 12;
+        }
+
+        // TODO: turn into enum
+        u32 attributes() const
+        {
+            return m_attributes;
+        }
+    private:
+        u32   m_attributes       : 12;
+        ptr_t m_physical_address : 20;
+    };
+}
