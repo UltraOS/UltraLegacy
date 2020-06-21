35f5c4cbd37af69dd203c03e8d3d5a2b74e108f0
8infy <99danilt@gmail.com>
2020-06-21 18:08:10 +0300

    [Kernel/Memory] Added Page.cpp and a bunch of useful utilities.

---
+++ Kernel/Memory/Page.cpp
@@ -0,0 +1,20 @@
+#include "Page.h"
+#include "MemoryManager.h"
+
+namespace kernel {
+
+    Page::Page(ptr_t physical_address)
+        : m_physical_address(physical_address)
+    {
+    }
+    
+    ptr_t Page::address() const
+    {
+        return m_physical_address;
+    }
+    
+    Page::~Page()
+    {
+        MemoryManager::the().free_page(*this);
+    }
+}
--- Kernel/Memory/Page.h
+++ Kernel/Memory/Page.h
@@ -1,31 +1,30 @@
 #pragma once
 
 #include "Common/Types.h"
-#include "MemoryManager.h"
+#include "Core/Runtime.h"
 
 namespace kernel {
 
     class Page
     {
     public:
-        static constexpr size_t size = 4096;
+        static constexpr size_t size           = 4096;
+        static constexpr ptr_t  alignment_mask = 0xFFFFF000;
 
-        Page(ptr_t physical_address)
-            : m_physical_address(physical_address)
+        Page(ptr_t physical_address);
+
+        ptr_t address() const;
+
+        static constexpr bool is_aligned(ptr_t address)
         {
+            return (address % size) == 0;
         }
 
-        ptr_t address() const
-        {
-            return m_physical_address;
-        }
-
-        ~Page()
-        {
-            MemoryManager::the().free_page(*this);
-        }
+        ~Page();
 
     private:
         ptr_t m_physical_address;
     };
+
+    #define ASSERT_PAGE_ALIGNED(address) ASSERT(Page::is_aligned(address));
 }
