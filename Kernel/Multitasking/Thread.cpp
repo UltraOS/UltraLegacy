df6658b6c5dcb281d3966ec1925814b87d0d7fec
8infy <99danilt@gmail.com>
2020-06-28 17:51:37 +0300

    [Kernel/Mutitasking] A basic version of the Thread class.

--- Kernel/Multitasking/Thread.cpp
+++ Kernel/Multitasking/Thread.cpp
@@ -2,6 +2,7 @@
 
 namespace kernel {
 
+u32  Thread::s_next_thread_id;
 TSS* Thread::s_tss;
 
 void Thread::initialize()
@@ -9,29 +10,71 @@ void Thread::initialize()
     s_tss = new TSS;
 }
 
-Thread::Thread(PageDirectory* page_directory, ptr_t kernel_stack_top, ptr_t eip)
-    : m_control_block { kernel_stack_top - 20, page_directory }
+RefPtr<Thread> Thread::create_supervisor_thread(Address kernel_stack, Address entrypoint)
 {
-    *reinterpret_cast<u32*>(kernel_stack_top - 4)  = eip;
-    *reinterpret_cast<u32*>(kernel_stack_top - 8)  = kernel_stack_top;
-    *reinterpret_cast<u32*>(kernel_stack_top - 12) = kernel_stack_top;
-    *reinterpret_cast<u32*>(kernel_stack_top - 16) = kernel_stack_top;
-    *reinterpret_cast<u32*>(kernel_stack_top - 20) = kernel_stack_top;
+    Address adjusted_stack = kernel_stack - sizeof(supervisor_starting_stack_frame);
+
+    auto thread                        = new Thread(PageDirectory::of_kernel(), adjusted_stack);
+    thread->m_is_supervisor            = true;
+    thread->m_initial_kernel_stack_top = kernel_stack;
+
+    auto* frame = new (adjusted_stack.as_pointer<supervisor_starting_stack_frame>()) supervisor_starting_stack_frame;
+    auto& switcher_frame = frame->switcher_frame;
+
+    switcher_frame.instruction_pointer = entrypoint;
+    switcher_frame.ebx                 = 0xDEADC0DE;
+    switcher_frame.esi                 = 0xDEADBEEF;
+    switcher_frame.edi                 = 0xC0DEBEEF;
+    switcher_frame.ebp                 = 0x00000000;
+
+    return thread;
 }
 
-// TODO: this should be private as it's only an acceptible constructor for the kernel's main thread
-Thread::Thread(PageDirectory* page_directory, ptr_t kernel_stack_top)
-    : m_control_block { kernel_stack_top, page_directory }
+RefPtr<Thread>
+Thread::create_user_thread(PageDirectory& page_dir, Address user_stack, Address kernel_stack, Address entrypoint)
+{
+    Address adjusted_stack = kernel_stack - sizeof(userland_starting_stack_frame);
+
+    auto thread                        = new Thread(page_dir, kernel_stack);
+    thread->m_initial_kernel_stack_top = kernel_stack;
+
+    auto* frame      = new (adjusted_stack.as_pointer<userland_starting_stack_frame>()) userland_starting_stack_frame;
+    auto& iret_frame = frame->interrupt_frame;
+    auto& switcher_frame = frame->switcher_frame;
+
+    static constexpr auto interrupts_enabled_flag = 0x200;
+    static constexpr auto rpl_ring_3              = 0x3;
+
+    iret_frame.data_selector       = GDT::userland_data_selector() | rpl_ring_3;
+    iret_frame.stack_pointer       = user_stack;
+    iret_frame.eflags              = interrupts_enabled_flag;
+    iret_frame.data_selector       = GDT::userland_code_selector() | rpl_ring_3;
+    iret_frame.instruction_pointer = entrypoint;
+
+    switcher_frame.instruction_pointer = userland_entrypoint;
+    switcher_frame.ebx                 = 0xDEADC0DE;
+    switcher_frame.esi                 = 0xDEADBEEF;
+    switcher_frame.edi                 = 0xC0DEBEEF;
+    switcher_frame.ebp                 = 0x00000000;
+
+    return thread;
+}
+
+Thread::Thread(PageDirectory& page_dir, Address kernel_stack)
+    : m_thread_id(s_next_thread_id++), m_page_directory(page_dir), m_control_block { kernel_stack }
 {
 }
 
-void Thread::set_next(Thread* thread)
+void Thread::activate()
 {
-    m_next_thread = thread;
+    m_state = State::RUNNING;
+
+    if (is_user())
+        s_tss->set_kernel_stack_pointer(m_initial_kernel_stack_top);
 }
 
-Thread* Thread::get_next()
+void Thread::deactivate()
 {
-    return m_next_thread;
+    m_state = State::READY;
 }
 }
--- Kernel/Multitasking/Thread.h
+++ Kernel/Multitasking/Thread.h
@@ -5,27 +5,90 @@
 
 namespace kernel {
 
+using userland_entrypoint_t = void (*)();
+
+// defined in Core/crt0.asm
+extern "C" userland_entrypoint_t userland_entrypoint;
+
 class Thread {
 public:
-    struct ControlBlock {
-        ptr_t          kernel_stack_top;
-        PageDirectory* page_directory;
+    friend class Process;
+
+    enum class State {
+        RUNNING,
+        DEAD,
+        BLOCKED,
+        READY,
     };
 
-    Thread(PageDirectory* page_directory, ptr_t kernel_stack_top);
-    Thread(PageDirectory* page_directory, ptr_t kernel_stack_top, ptr_t eip);
+    struct ControlBlock {
+        Address current_kernel_stack_top;
+    };
+
     static void initialize();
 
-    void    set_next(Thread*);
-    Thread* get_next();
+    static RefPtr<Thread> create_supervisor_thread(Address kernel_stack, Address entrypoint);
+
+    static RefPtr<Thread>
+    create_user_thread(PageDirectory& page_dir, Address user_stack, Address kernel_stack, Address entrypoint);
+
+    void    set_next(Thread* thread) { m_next = thread; }
+    Thread* next() { return m_next; }
+    bool    has_next() { return next(); }
+
+    void activate();
+    void deactivate();
+
+    State state() const { return m_state; }
 
     ControlBlock* control_block() { return &m_control_block; }
 
+    bool is_supervisor() const { return m_is_supervisor; }
+    bool is_user() const { return !is_supervisor(); }
+
 private:
-    Thread* m_next_thread;
+    struct iret_stack_frame {
+        Address instruction_pointer;
+        u32     code_selector;
+        u32     eflags;
+        Address stack_pointer;
+        u32     data_selector;
+    };
 
-    ControlBlock m_control_block;
+    struct task_switcher_task_frame {
+        u32     ebp;
+        u32     edi;
+        u32     esi;
+        u32     ebx;
+        Address instruction_pointer;
+    };
 
+    struct supervisor_starting_stack_frame {
+        task_switcher_task_frame switcher_frame;
+    };
+
+    struct userland_starting_stack_frame {
+        iret_stack_frame         interrupt_frame;
+        task_switcher_task_frame switcher_frame;
+    };
+
+    static_assert(sizeof(iret_stack_frame) == 20);
+    static_assert(sizeof(task_switcher_task_frame) == 20);
+    static_assert(sizeof(supervisor_starting_stack_frame) == 20);
+    static_assert(sizeof(userland_starting_stack_frame) == 40);
+
+    Thread(PageDirectory& page_dir, Address kernel_stack);
+
+private:
+    u32            m_thread_id;
+    PageDirectory& m_page_directory;
+    ControlBlock   m_control_block { nullptr };
+    Address        m_initial_kernel_stack_top { nullptr };
+    State          m_state { State::READY };
+    bool           m_is_supervisor { false };
+    Thread*        m_next { nullptr };
+
+    static u32  s_next_thread_id;
     static TSS* s_tss;
 };
 }
