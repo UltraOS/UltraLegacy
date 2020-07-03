777abc027b1bdb99b3c775cc2969f4084fa6496a
8infy <99danilt@gmail.com>
2020-07-03 20:12:50 +0300

    [Kernel/Interrupts] Added enable/disable irq for IRQHandler.

--- Kernel/Interrupts/IRQHandler.h
+++ Kernel/Interrupts/IRQHandler.h
@@ -10,14 +10,17 @@ namespace kernel {
 
 class IRQHandler {
 public:
-    IRQHandler(u16 irq_index) : m_irq_index(irq_index) { IRQManager::the().register_irq_handler(*this); }
+    IRQHandler(u16 irq_index) : m_irq_index(irq_index) { IRQManager::register_irq_handler(*this); }
 
     u16 irq_index() const { return m_irq_index; }
 
     virtual void on_irq(const RegisterState& registers) = 0;
     virtual void finialize_irq() { PIC::end_of_interrupt(irq_index()); }
 
-    virtual ~IRQHandler() { IRQManager::the().unregister_irq_handler(*this); }
+    virtual void enable_irq() { PIC::enable_irq(irq_index()); }
+    virtual void disable_irq() { PIC::disable_irq(irq_index()); }
+
+    virtual ~IRQHandler() { IRQManager::unregister_irq_handler(*this); }
 
 private:
     u16 m_irq_index;
