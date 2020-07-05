474db544125e9e6bd80e28ef56d8d0587bc31d2d
8infy <99danilt@gmail.com>
2020-07-05 12:46:51 +0300

    [Kernel/Multitasking] Added Sleep.h

---
+++ Kernel/Multitasking/Sleep.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Common/Types.h"
+#include "Interrupts/Common.h"
+#include "Interrupts/Timer.h"
+#include "Thread.h"
+#include "Scheduler.h"
+
+namespace kernel::sleep {
+
+    void until(u64 time)
+    {
+        cli();
+
+        if (time <= Timer::nanoseconds_since_boot())
+            return;
+
+        Thread::current()->sleep(time);
+        Scheduler::yield();
+        sti();
+    }
+
+    inline void for_nanoseconds(u64 time)
+    {
+        until(Timer::nanoseconds_since_boot() + time);
+    }
+
+    inline void for_seconds(u64 time)
+    {
+        until(Timer::nanoseconds_since_boot() + (time * Timer::nanoseconds_in_second));
+    }
+}
