e647e42a82415b049eaf1f158af3eac0b6ef9e40
8infy <99danilt@gmail.com>
2020-07-09 22:24:35 +0300

    [Scripts] Added a few settings for QEMU.

--- run_qemu.sh
+++ run_qemu.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 /bin/bash Scripts/build_ultra.sh || exit 1
-qemu-system-i386 -drive file="Images/UltraHDD.vmdk",index=0,media=disk -debugcon stdio
+qemu-system-i386 -drive file="Images/UltraHDD.vmdk",index=0,media=disk \
+                 -debugcon stdio                                       \
+                 -cpu core2duo                                         \
+                 -smp 2                                                \
+                 -m 128                                                \
+                 -no-reboot                                            \
