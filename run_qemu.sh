f13170635eb555b5ac75c1280050cd2f55d1fd03
8infy <99danilt@gmail.com>
2020-07-11 00:33:42 +0300

    [Scripts] Removed an unnecessary option from run_qemu.sh

--- run_qemu.sh
+++ run_qemu.sh
@@ -3,7 +3,6 @@
 /bin/bash Scripts/build_ultra.sh || exit 1
 qemu-system-i386 -drive file="Images/UltraHDD.vmdk",index=0,media=disk \
                  -debugcon stdio                                       \
-                 -cpu core2duo                                         \
                  -smp 2                                                \
                  -m 128                                                \
                  -no-reboot                                            \
