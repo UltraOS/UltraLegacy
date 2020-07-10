c5ac3ff1c86bc064dfb363b6d4e0d93b5d331040
8infy <99danilt@gmail.com>
2020-07-11 00:31:03 +0300

    [Kernel/Interrupts] Added an abstract InterruptController class.

---
+++ Kernel/Interrupts/InterruptController.cpp
@@ -0,0 +1,30 @@
+#include "InterruptController.h"
+#include "LAPIC.h"
+#include "Core/CPU.h"
+#include "PIC.h"
+
+namespace kernel {
+
+InterruptController* InterruptController::s_instance;
+
+void InterruptController::discover_and_initialize()
+{
+    if (CPU::supports_smp()) {
+        log() << "InteruptController: SMP support detected. Initializing LAPIC and IOAPIC...";
+        PIC::ensure_disabled();
+        LAPIC::set_base_address(CPU::smp_data().local_apic_address);
+        LAPIC::initialize_for_this_processor();
+        // s_instance = new IOAPIC;
+    } else {
+        log() << "InterruptController: No SMP support detected, reverting to PIC";
+        s_instance = new PIC;
+    }
+}
+
+InterruptController& InterruptController::the()
+{
+    ASSERT(s_instance != nullptr);
+
+    return *s_instance;
+}
+}
---
+++ Kernel/Interrupts/InterruptController.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Common/Types.h"
+
+namespace kernel {
+
+class InterruptController {
+public:
+static void discover_and_initialize();
+
+static InterruptController& the();
+
+virtual void end_of_interrupt(u8 request_number) = 0;
+
+virtual void clear_all() = 0;
+
+virtual void enable_irq(u8 index) = 0;
+virtual void disable_irq(u8 index) = 0;
+
+virtual bool is_spurious(u8 request_number) = 0;
+virtual void handle_spurious_irq(u8 request_number) = 0;
+
+private:
+    static InterruptController* s_instance;
+};
+}
