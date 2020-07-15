35d074fdf6cfc5e7d31d173432ead0dd6444297a
8infy <99danilt@gmail.com>
2020-07-15 21:45:33 +0300

    [Scripts] Added run_qemu.bat

---
+++ run_qemu.bat
@@ -0,0 +1,9 @@
+@echo off
+call Scripts\build_ultra.bat || goto error
+qemu-system-i386 -drive file="Images/UltraHDD.vmdk",index=0,media=disk -debugcon stdio -smp 2 -m 128 -no-reboot
+exit 0
+
+:error
+popd
+exit 1
+
