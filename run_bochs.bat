c628fa9e58cb460741ab3ad919530c54c6042baf
8infy <99danilt@gmail.com>
2020-07-15 21:44:50 +0300

    [Scripts] Enabled SMP for bochs.

--- run_bochs.bat
+++ run_bochs.bat
@@ -3,7 +3,8 @@ call Scripts\build_ultra.bat || goto error
 pushd %~dp0
 set images=%CD%\Images
 popd
-bochs -unlock -f Scripts\bochsrc.bxrc
+del ".\Images\UltraHDD-flat.vmdk.lock" 2>NUL
+bochs.exe -f Scripts\bochsrc.bxrc
 exit 0
 
 :error
--- run_bochs_debug.bat
+++ run_bochs_debug.bat
@@ -3,8 +3,9 @@ call Scripts\build_ultra.bat || goto error
 pushd %~dp0
 set images=%CD%\Images
 popd
-bochsdbg -unlock -f Scripts\bochsrc_debug.bxrc
-del bx_enh_dbg.ini
+del ".\Images\UltraHDD-flat.vmdk.lock" 2>NUL
+bochsdbg.exe -f Scripts\bochsrc_debug.bxrc
+del bx_enh_dbg.ini 2>NUL
 exit 0
 
 :error
