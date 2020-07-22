e270bace967cfeacc2b0c2b4ab79ea0dd1059345
8infy <99danilt@gmail.com>
2020-07-22 21:00:01 +0300

    [Scripts] Now using x86-64 bit QEMU for any configuration.

--- run_qemu.bat
+++ run_qemu.bat
@@ -1,6 +1,6 @@
 @echo off
 call Scripts\build_ultra.bat || goto error
-qemu-system-i386 -drive file="Images/UltraHDD.vmdk",index=0,media=disk -debugcon stdio -smp 2 -m 128 -no-reboot
+qemu-system-x86_64 -drive file="Images/UltraHDD.vmdk",index=0,media=disk -debugcon stdio -smp 2 -m 128 -no-reboot
 exit 0
 
 :error
--- run_qemu.sh
+++ run_qemu.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 /bin/bash Scripts/build_ultra.sh || exit 1
-qemu-system-i386 -drive file="Images/UltraHDD.vmdk",index=0,media=disk \
-                 -debugcon stdio                                       \
-                 -smp 2                                                \
-                 -m 128                                                \
-                 -no-reboot                                            \
+qemu-system-x86_64 -drive file="Images/UltraHDD.vmdk",index=0,media=disk \
+                   -debugcon stdio                                       \
+                   -smp 2                                                \
+                   -m 128                                                \
+                   -no-reboot                                            \
