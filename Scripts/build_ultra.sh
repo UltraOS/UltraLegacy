867ee6e66a57fb3f847f582bd7d2f3be5e8f3409
8infy <99danilt@gmail.com>
2020-07-25 22:14:35 +0300

    [Project] Separated builds by architecture. Added some x86-64 flags.

--- Scripts/build_ultra.sh
+++ Scripts/build_ultra.sh
@@ -10,11 +10,24 @@ echo "Building UltraOS..."
 true_path="$(dirname "$(realpath "$0")")"
 root_path=$true_path/..
 
+arch="32"
+
+if [ "$1" ]
+  then
+    if [ $1 != "64" ] && [ $1 != "32" ]
+    then
+      echo "Unknown architecture $1"
+      on_error
+    else
+      arch="$1"
+    fi
+fi
+
 source $true_path/utils.sh
 
 pushd $root_path
-mkdir -p Build || on_error
-pushd Build
+mkdir -p Build$arch || on_error
+pushd Build$arch
 cmake .. -DARCH=$1 || on_error
 cmake --build . || on_error
 popd
