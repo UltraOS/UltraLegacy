f169f5ec6bca3643abec16cbd4831c7881651655
8infy <99danilt@gmail.com>
2020-08-09 00:07:06 +0300

    [Kernel/Core] Added CMOS.h

---
+++ Kernel/Core/CMOS.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Common/Types.h"
+#include "IO.h"
+
+namespace kernel {
+
+class CMOS {
+public:
+    static constexpr u8 selector_port = 0x70;
+    static constexpr u8 register_port = 0x71;
+
+    template <u8 register_index>
+    static u8 read()
+    {
+        IO::out8<selector_port>(register_index);
+        return IO::in8<register_port>();
+    }
+
+    template <u8 register_index>
+    static void write(u8 value)
+    {
+        IO::out8<selector_port>(register_index);
+        IO::out8<register_index>(value);
+    }
+};
+}
