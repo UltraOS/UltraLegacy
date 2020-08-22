c010caebee8242ddca7b39a66ca6ee746422ea9c
8infy <99danilt@gmail.com>
2020-08-22 22:52:41 +0300

    [Kernel] Improved a few glyphs, fixed formatting.

--- Kernel/Drivers/Video/GenericVideoDevice.h
+++ Kernel/Drivers/Video/GenericVideoDevice.h
@@ -15,7 +15,7 @@ public:
 
     void draw_at(size_t x, size_t y, RGBA pixel) override;
 
-    Address framebuffer() { return m_mode.framebuffer; }
+    Address framebuffer() override { return m_mode.framebuffer; }
     size_t  bpp() const override { return m_mode.pitch; }
     size_t  pitch() const override { return m_mode.pitch; }
     size_t  width() const override { return m_mode.width; }
