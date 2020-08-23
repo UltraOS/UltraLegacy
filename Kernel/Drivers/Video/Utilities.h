716ef8bc779ef3c186fec229a0889e0df76190cb
8infy <99danilt@gmail.com>
2020-08-23 21:19:00 +0300

    [Kernel/Drivers] Added basic Rect class.

--- Kernel/Drivers/Video/Utilities.h
+++ Kernel/Drivers/Video/Utilities.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Common/Types.h"
+#include "Common/Pair.h"
 
 namespace kernel {
 
@@ -15,5 +16,23 @@ struct RGBA {
     static constexpr RGBA black() { return { 0x00, 0x00, 0x00, 0xFF }; }
 
     u32 as_u32() { return *reinterpret_cast<u32*>(this); }
+};
+
+class Rect {
+public:
+    Rect(size_t top_left_x, size_t top_left_y, size_t width, size_t height)
+        : m_x(top_left_x), m_y(top_left_y), m_width(width), m_height(height) {}
+
+    Pair<size_t, size_t> top_left() const  { return { m_x, m_y }; }
+    Pair<size_t, size_t> bottom_right() const { return { m_x + m_width, m_y + m_height }; }
+
+    size_t width() const { return m_width; }
+    size_t height() const { return m_height; }
+
+private:
+    size_t m_x;
+    size_t m_y;
+    size_t m_width;
+    size_t m_height;
 };
 }
