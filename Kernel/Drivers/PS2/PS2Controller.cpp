ec58dd30812cda585a1bcb9907f377db4358dfa4
8infy <99danilt@gmail.com>
2020-09-03 21:04:28 +0300

    [Kernel/Drivers] Finished the base PS2Controller driver.

--- Kernel/Drivers/PS2/PS2Controller.cpp
+++ Kernel/Drivers/PS2/PS2Controller.cpp
@@ -8,12 +8,237 @@ PS2Controller* PS2Controller::s_instance;
 void PS2Controller::initialize()
 {
     s_instance = new PS2Controller();
+    s_instance->discover_all_devices();
 }
 
-PS2Controller::PS2Controller()
+void PS2Controller::discover_all_devices()
 {
-    // initialize...
-    // discover all devices...
-    m_devices.emplace(new PS2Keyboard());
+    // TODO: disable USB Legacy Support
+    // TODO: check FADT to make sure PS2 controller exists
+
+    // disable ports
+    send_command(Command::DISABLE_PORT_1);
+    send_command(Command::DISABLE_PORT_2);
+
+    flush();
+
+    // disable both irqs and translation
+    auto config = read_configuration();
+    config.port_1_irq_enabled = false;
+    config.port_2_irq_enabled = false;
+    config.port_1_translation_enabled = false;
+
+    // potentially
+    bool is_dual_channel = config.port_2_clock_disabled;
+
+    write_configuration(config);
+
+    // perform controller tests
+    send_command(Command::TEST_CONTROLLER);
+    static constexpr u8 controller_test_passed = 0x55;
+    auto controller_test_result = read_data();
+
+    if (controller_test_result != controller_test_passed) {
+        error() << "PS2Controller: self test failed, expected: 0x55 received: " << format::as_hex << controller_test_result;
+        hang();
+    }
+
+    // restore the configuration again because it might've reset
+    write_configuration(config);
+
+    // check if it's actually a dual channel
+    if (is_dual_channel) {
+        send_command(Command::ENABLE_PORT_2);
+        auto config = read_configuration();
+        is_dual_channel = !config.port_2_clock_disabled;
+
+        if (is_dual_channel)
+            send_command(Command::DISABLE_PORT_2);
+    }
+
+    // perform channel tests
+    static constexpr u8 port_test_passed = 0x00;
+
+    send_command(Command::TEST_PORT_1);
+    auto port1_test_result = read_data();
+    bool port1_passed = port1_test_result == port_test_passed;
+
+    u8 port2_test_result = 0xFF;
+    bool port2_passed = false;
+
+    if (is_dual_channel) {
+        send_command(Command::TEST_PORT_2);
+        port2_test_result = read_data();
+        port2_passed = port2_test_result == port_test_passed;
+    }
+
+    // Initialize all available devices
+    if (port1_passed) {
+        send_command(Command::ENABLE_PORT_1);
+
+        if (!reset_device(Channel::ONE)) {
+            log() << "PS2Controller: Port 1 doesn't have a device attached";
+        }
+
+        discover_device(Channel::ONE);
+    } else
+        warning() << "PS2Controller: port 1 failed self test, expected: 0x00 received: " << format::as_hex << port1_test_result;
+
+    if (port2_passed) {
+        send_command(Command::ENABLE_PORT_1);
+
+        if (!reset_device(Channel::TWO)) {
+            log() << "PS2Controller: Port 2 doesn't have a device attached";
+        }
+
+        discover_device(Channel::TWO);
+    } else if (is_dual_channel && !port2_passed)
+        warning() << "PS2Controller: port 2 failed self test, expected: 0x00 received: " << format::as_hex << port2_test_result;
+
+    if (!port1_passed && !port2_passed) {
+        error() << "PS2Controller: All ports failed self test!";
+        hang();
+    }
+}
+
+bool PS2Controller::reset_device(Channel channel)
+{
+    send_command_to_device(channel, DeviceCommand::RESET, false);
+
+    u8 device_response[3];
+    device_response[0] = read_data(true);
+    device_response[1] = read_data(true);
+    device_response[2] = read_data(true);
+
+    if ((device_response[0] == command_ack && device_response[1] == self_test_passed) ||
+        (device_response[0] == self_test_passed && device_response[1] == command_ack)) {
+        return true;
+    } else {
+        if (!did_last_read_timeout()) {
+            error() << "PS2Controller: Device " << static_cast<u8>(channel)
+                    << " is present but responded with unknown sequence -> "
+                    << format::as_hex << device_response[0] << "-" << device_response[1];
+            hang();
+        }
+
+        log() << "PS2Controller: Device " << static_cast<u8>(channel) << " is not present.";
+        return false;
+    }
+}
+
+void PS2Controller::discover_device(Channel channel)
+{
+    // identify device
+    send_command_to_device(channel, DeviceCommand::DISABLE_SCANNING);
+    send_command_to_device(channel, DeviceCommand::IDENTIFY);
+
+    u8 response_byte_count = 0;
+
+    read_data(true);
+    response_byte_count += !!did_last_read_timeout();
+
+    read_data(true);
+    response_byte_count += !!did_last_read_timeout();
+
+    switch (response_byte_count)
+    {
+    case 0:
+    case 2:
+        log() << "Detected a keyboard on channel " << static_cast<u8>(channel);
+        initialzie_keyboard(channel);
+        return;
+    case 1:
+        log() << "Detected a mouse on channel " << static_cast<u8>(channel);
+        initialize_mouse(channel);
+        return;
+    }
+}
+
+void PS2Controller::initialize_mouse(Channel channel)
+{
+    // detect the actual mouse type and initialize
+    (void)channel;
+}
+
+void PS2Controller::initialzie_keyboard(Channel channel)
+{
+    // detect the actual keyboard type and initialize
+    m_devices.emplace(new PS2Keyboard(channel));
+}
+
+bool PS2Controller::should_resend()
+{
+    auto data = read_data();
+
+    if (data == command_ack)
+        return false;
+
+    if (data == resend_command)
+        return true;
+
+     error() << "PS2Controller: unexpected data instead of ACK/resend -> " << format::as_hex << data;
+     hang();
+}
+
+void PS2Controller::send_command_to_device(Channel channel, DeviceCommand command, bool should_expect_ack)
+{
+    send_command_to_device(channel, static_cast<u8>(command), should_expect_ack);
+}
+
+void PS2Controller::send_command_to_device(Channel channel, u8 command, bool should_expect_ack)
+{
+    size_t resend_counter = 1000;
+
+    do {
+        switch (channel) {
+        case Channel::TWO:
+            send_command(Command::WRITE_PORT_2);
+            [[fallthrough]];
+        case Channel::ONE:
+            write<data_port>(command);
+            break;
+        default:
+            error() << "PS2Controller: Unknown channel " << static_cast<u8>(channel);
+            hang();
+        }
+    } while (should_expect_ack && --resend_counter && should_resend());
+
+    if (should_expect_ack && !resend_counter) {
+        error() << "PS2Controller: failed to receive command ACK after 1000 attempts";
+        hang();
+    }
+}
+
+PS2Controller::Status PS2Controller::status()
+{
+    auto raw_status = IO::in8<status_port>();
+
+    return *reinterpret_cast<Status*>(&raw_status);
+}
+
+PS2Controller::Configuration PS2Controller::read_configuration()
+{
+    send_command(Command::READ_CONFIGURATION);
+
+    auto config = read_data();
+
+    return *reinterpret_cast<Configuration*>(&config);
+}
+
+void PS2Controller::write_configuration(Configuration config)
+{
+    send_command(Command::WRITE_CONFIGURATION);
+    write<data_port>(*reinterpret_cast<u8*>(&config));
+}
+
+void PS2Controller::send_command(Command command)
+{
+    write<command_port>(static_cast<u8>(command));
+}
+
+void PS2Controller::flush()
+{
+    while (status().output_full)
+        IO::in8<data_port>();
 }
 }
--- Kernel/Drivers/PS2/PS2Controller.h
+++ Kernel/Drivers/PS2/PS2Controller.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "Core/IO.h"
+#include "Common/Logger.h"
+
 #include "Drivers/Device.h"
 #include "Common/DynamicArray.h"
 
@@ -7,10 +10,61 @@ namespace kernel {
 
 class PS2Controller : public Device {
 public:
+    static constexpr u8 data_port    = 0x60;
+    static constexpr u8 status_port  = 0x64;
+    static constexpr u8 command_port = 0x64;
+
+    enum class Command : u8 {
+        READ_CONFIGURATION  = 0x20,
+        WRITE_CONFIGURATION = 0x60,
+        DISABLE_PORT_2      = 0xA7,
+        ENABLE_PORT_2       = 0xA8,
+        TEST_PORT_2         = 0xA9,
+        TEST_CONTROLLER     = 0xAA,
+        TEST_PORT_1         = 0xAB,
+        DISABLE_PORT_1      = 0xAD,
+        ENABLE_PORT_1       = 0xAE,
+        WRITE_PORT_2        = 0xD4,
+    };
+
+    static constexpr u8 self_test_passed = 0xAA;
+    static constexpr u8 command_ack      = 0xFA;
+    static constexpr u8 resend_command   = 0xFE;
+
+    enum class DeviceCommand : u8 {
+        RESET            = 0xFF,
+        ENABLE_SCANNING  = 0xF4,
+        DISABLE_SCANNING = 0xF5,
+        IDENTIFY         = 0xF2,
+    };
+
+    struct Configuration {
+        bool port_1_irq_enabled         : 1;
+        bool port_2_irq_enabled         : 1;
+        bool system_flag                : 1;
+        bool unused_1                   : 1;
+        bool port_1_clock_disabled      : 1;
+        bool port_2_clock_disabled      : 1;
+        bool port_1_translation_enabled : 1;
+        bool unused_2                   : 1;
+    };
+
+    struct Status {
+         bool output_full     : 1;
+         bool input_full      : 1;
+         bool system_flag     : 1;
+         bool controller_data : 1;
+         bool unknown_1       : 1;
+         bool unknown_2       : 1;
+         bool timeout_error   : 1;
+         bool parity_error    : 1;
+    };
+
+    static_assert(sizeof(Configuration) == 1);
+    static_assert(sizeof(Status)        == 1);
+
     Type type() const override { return Type::CONTROLLER; }
     StringView name() const override { return "8042 PS/2 Controller"; }
-
-    PS2Controller();
 
     static void initialize();
 
@@ -21,8 +75,74 @@ public:
         return *s_instance;
     }
 
+    void send_command(Command);
+    void flush();
+
+    Configuration read_configuration();
+    void write_configuration(Configuration);
+
+    Status status();
+
+    enum class Channel { ONE = 1, TWO = 2 };
+
+    void send_command_to_device(Channel, DeviceCommand, bool should_expect_ack = true);
+    void send_command_to_device(Channel, u8, bool should_expect_ack = true);
+
+    bool should_resend();
+
+    template<u8 port>
+    void write(u8 data)
+    {
+        size_t timeout_counter = 100000;
+
+        while (status().input_full && --timeout_counter)
+            pause();
+
+        if (timeout_counter == 0) {
+            error() << "PS2Controller: unexpected write timeout!";
+            hang();
+        }
+
+        IO::out8<port>(data);
+    }
+
+    u8 read_data(bool allow_failure = false)
+    {
+        size_t timeout_counter = 100000;
+
+        while (!status().output_full && --timeout_counter)
+            pause();
+
+        if (timeout_counter == 0) {
+            if (allow_failure) {
+                m_last_read_timeout = true;
+                return 0x00;
+            }
+
+            error() << "PS2Controller: unexpected read timeout!";
+            hang();
+        }
+
+        if (allow_failure)
+            m_last_read_timeout = false;
+
+        return IO::in8<data_port>();
+    }
+
+    bool did_last_read_timeout() { return m_last_read_timeout; }
+
+private:
+    void discover_all_devices();
+
+    void discover_device(Channel);
+    bool reset_device(Channel);
+
+    void initialize_mouse(Channel);
+    void initialzie_keyboard(Channel);
+
 private:
     DynamicArray<Device*> m_devices;
+    bool m_last_read_timeout;
 
     static PS2Controller* s_instance;
 };
