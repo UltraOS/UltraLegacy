c1271550e40a8334aa837955e0a4d6c11b49cba3
8infy <99danilt@gmail.com>
2020-09-06 16:31:41 +0300

    [Kernel/Drivers] Added Key.h that defines all virtual keys.

---
+++ Kernel/Drivers/Key.h
@@ -0,0 +1,140 @@
+#pragma once
+
+#include "Common/String.h"
+
+#define ENUMERATE_VIRTUAL_KEYS                                                                                         \
+    VIRTUAL_KEY(UNKNOWN, "Unknown")                                                                                    \
+    VIRTUAL_KEY(ESC, "Escape")                                                                                         \
+    VIRTUAL_KEY(F1, "F1")                                                                                              \
+    VIRTUAL_KEY(F2, "F2")                                                                                              \
+    VIRTUAL_KEY(F3, "F3")                                                                                              \
+    VIRTUAL_KEY(F4, "F4")                                                                                              \
+    VIRTUAL_KEY(F5, "F5")                                                                                              \
+    VIRTUAL_KEY(F6, "F6")                                                                                              \
+    VIRTUAL_KEY(F7, "F7")                                                                                              \
+    VIRTUAL_KEY(F8, "F8")                                                                                              \
+    VIRTUAL_KEY(F9, "F9")                                                                                              \
+    VIRTUAL_KEY(F10, "F10")                                                                                            \
+    VIRTUAL_KEY(F11, "F11")                                                                                            \
+    VIRTUAL_KEY(F12, "F12")                                                                                            \
+    VIRTUAL_KEY(PRT_SC, "Print Screen")                                                                                \
+    VIRTUAL_KEY(SCR_LK, "Scroll Lock:")                                                                                \
+    VIRTUAL_KEY(PAUSE_BREAK, "Pause Break")                                                                            \
+    VIRTUAL_KEY(BACKTICK, "`")                                                                                         \
+    VIRTUAL_KEY(ONE, "1")                                                                                              \
+    VIRTUAL_KEY(TWO, "2")                                                                                              \
+    VIRTUAL_KEY(THREE, "3")                                                                                            \
+    VIRTUAL_KEY(FOUR, "4")                                                                                             \
+    VIRTUAL_KEY(FIVE, "5")                                                                                             \
+    VIRTUAL_KEY(SIX, "6")                                                                                              \
+    VIRTUAL_KEY(SEVEN, "7")                                                                                            \
+    VIRTUAL_KEY(EIGHT, "8")                                                                                            \
+    VIRTUAL_KEY(NINE, "9")                                                                                             \
+    VIRTUAL_KEY(ZERO, "0")                                                                                             \
+    VIRTUAL_KEY(HYPHEN, "-")                                                                                           \
+    VIRTUAL_KEY(EQUAL, "=")                                                                                            \
+    VIRTUAL_KEY(BACKSPACE, "Backspace")                                                                                \
+    VIRTUAL_KEY(INSERT, "Insert")                                                                                      \
+    VIRTUAL_KEY(HOME, "Home")                                                                                          \
+    VIRTUAL_KEY(PAGE_UP, "Page Up")                                                                                    \
+    VIRTUAL_KEY(NUM_LK, "Num Lock")                                                                                    \
+    VIRTUAL_KEY(NP_FORWARD_SLASH, "/")                                                                                 \
+    VIRTUAL_KEY(NP_ASTERISK, "*")                                                                                      \
+    VIRTUAL_KEY(NP_HYPHEN, "-")                                                                                        \
+    VIRTUAL_KEY(TAB, "Tab")                                                                                            \
+    VIRTUAL_KEY(Q, "Q")                                                                                                \
+    VIRTUAL_KEY(W, "W")                                                                                                \
+    VIRTUAL_KEY(E, "E")                                                                                                \
+    VIRTUAL_KEY(R, "R")                                                                                                \
+    VIRTUAL_KEY(T, "T")                                                                                                \
+    VIRTUAL_KEY(Y, "Y")                                                                                                \
+    VIRTUAL_KEY(U, "U")                                                                                                \
+    VIRTUAL_KEY(I, "I")                                                                                                \
+    VIRTUAL_KEY(O, "O")                                                                                                \
+    VIRTUAL_KEY(P, "P")                                                                                                \
+    VIRTUAL_KEY(OPEN_BRACKET, "[")                                                                                     \
+    VIRTUAL_KEY(CLOSE_BRACKET, "]")                                                                                    \
+    VIRTUAL_KEY(BACKSLASH, "\\")                                                                                       \
+    VIRTUAL_KEY(PIPE, "|")                                                                                             \
+    VIRTUAL_KEY(DEL, "Del")                                                                                            \
+    VIRTUAL_KEY(END, "End")                                                                                            \
+    VIRTUAL_KEY(PAGE_DOWN, "Page Down")                                                                                \
+    VIRTUAL_KEY(NP_SEVEN, "7")                                                                                         \
+    VIRTUAL_KEY(NP_EIGHT, "8")                                                                                         \
+    VIRTUAL_KEY(NP_NINE, "9")                                                                                          \
+    VIRTUAL_KEY(NP_PLUS, "+")                                                                                          \
+    VIRTUAL_KEY(CAPS_LOCK, "Caps Lock")                                                                                \
+    VIRTUAL_KEY(A, "A")                                                                                                \
+    VIRTUAL_KEY(S, "S")                                                                                                \
+    VIRTUAL_KEY(D, "D")                                                                                                \
+    VIRTUAL_KEY(F, "F")                                                                                                \
+    VIRTUAL_KEY(G, "G")                                                                                                \
+    VIRTUAL_KEY(H, "H")                                                                                                \
+    VIRTUAL_KEY(J, "J")                                                                                                \
+    VIRTUAL_KEY(K, "K")                                                                                                \
+    VIRTUAL_KEY(L, "L")                                                                                                \
+    VIRTUAL_KEY(SEMICOLON, ";")                                                                                        \
+    VIRTUAL_KEY(APOSTROPHE, "'")                                                                                       \
+    VIRTUAL_KEY(ENTER, "Enter")                                                                                        \
+    VIRTUAL_KEY(NP_FOUR, "4")                                                                                          \
+    VIRTUAL_KEY(NP_FIVE, "5")                                                                                          \
+    VIRTUAL_KEY(NP_SIX, "6")                                                                                           \
+    VIRTUAL_KEY(LEFT_SHIFT, "Left Shift")                                                                              \
+    VIRTUAL_KEY(Z, "Z")                                                                                                \
+    VIRTUAL_KEY(X, "X")                                                                                                \
+    VIRTUAL_KEY(C, "C")                                                                                                \
+    VIRTUAL_KEY(V, "V")                                                                                                \
+    VIRTUAL_KEY(B, "B")                                                                                                \
+    VIRTUAL_KEY(N, "N")                                                                                                \
+    VIRTUAL_KEY(M, "M")                                                                                                \
+    VIRTUAL_KEY(COMMA, ",")                                                                                            \
+    VIRTUAL_KEY(DOT, ".")                                                                                              \
+    VIRTUAL_KEY(FORWARD_SLASH, "/")                                                                                    \
+    VIRTUAL_KEY(RIGHT_SHIFT, "Right Shift")                                                                            \
+    VIRTUAL_KEY(ARROW_UP, "Arrow Up")                                                                                  \
+    VIRTUAL_KEY(NP_ONE, "1")                                                                                           \
+    VIRTUAL_KEY(NP_TWO, "2")                                                                                           \
+    VIRTUAL_KEY(NP_THREE, "3")                                                                                         \
+    VIRTUAL_KEY(NP_ENTER, "Enter")                                                                                     \
+    VIRTUAL_KEY(LEFT_CTRL, "Left Ctrl")                                                                                \
+    VIRTUAL_KEY(LEFT_LOGO, "Left Logo")                                                                                \
+    VIRTUAL_KEY(LEFT_ALT, "Left Alt")                                                                                  \
+    VIRTUAL_KEY(SPACE, "Space")                                                                                        \
+    VIRTUAL_KEY(RIGHT_ALT, "Right Alt")                                                                                \
+    VIRTUAL_KEY(RIGHT_GUI, "Right Logo")                                                                               \
+    VIRTUAL_KEY(CONTEXT_MENU, "Context Menu")                                                                          \
+    VIRTUAL_KEY(RIGHT_CTRL, "Right Ctrl")                                                                              \
+    VIRTUAL_KEY(ARROW_LEFT, "Arrow Left")                                                                              \
+    VIRTUAL_KEY(ARROW_DOWN, "Arrow Down")                                                                              \
+    VIRTUAL_KEY(ARROW_RIGHT, "Arrow Right")                                                                            \
+    VIRTUAL_KEY(NP_ZERO, "0")                                                                                          \
+    VIRTUAL_KEY(NP_DOT, ".")                                                                                           \
+    VIRTUAL_KEY(MM_PREVIOUS_TRACK, "Previous Track")                                                                   \
+    VIRTUAL_KEY(MM_NEXT_TRACK, "Next Track")                                                                           \
+    VIRTUAL_KEY(MM_MUTE, "Mute")                                                                                       \
+    VIRTUAL_KEY(MM_CALCULATOR, "Calcualtor")                                                                           \
+    VIRTUAL_KEY(MM_PLAY, "Play")                                                                                       \
+    VIRTUAL_KEY(MM_STOP, "Stop")                                                                                       \
+    VIRTUAL_KEY(MM_VOLUME_DOWN, "Volume Down")                                                                         \
+    VIRTUAL_KEY(MM_VOLUME_UP, "Volume Up")                                                                             \
+    VIRTUAL_KEY(MM_WWW_HOME, "WWW Home")
+
+enum class Key : unsigned char {
+#define VIRTUAL_KEY(key, representation) key,
+    ENUMERATE_VIRTUAL_KEYS
+#undef VIRTUAL_KEY
+};
+
+namespace kernel {
+
+StringView to_string(Key k)
+{
+    switch (k) {
+#define VIRTUAL_KEY(key, representation) case Key::key: return representation##_sv;
+        ENUMERATE_VIRTUAL_KEYS
+#undef VIRTUAL_KEY
+    default: return "Unknown"_sv;
+    }
+}
+
+}
