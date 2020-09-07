ee47b084f48c0d51bc79c5536af7641981596d17
8infy <99danilt@gmail.com>
2020-09-07 21:51:29 +0300

    [Kernel/Drivers] Added Keyboard and Mouse packets.

--- Kernel/Drivers/Keyboard.h
+++ Kernel/Drivers/Keyboard.h
@@ -1,11 +1,18 @@
 #pragma once
 
 #include "Drivers/Device.h"
+#include "WindowManager/VirtualKey.h"
 
 namespace kernel {
 
 class Keyboard : public Device {
 public:
     Type type() const override { return Type::KEYBOARD; }
+
+    struct Packet {
+        VK key;
+
+        enum State : u8 { PRESS, RELEASE } state;
+    };
 };
 }
--- Kernel/Drivers/Mouse.h
+++ Kernel/Drivers/Mouse.h
@@ -7,5 +7,17 @@ namespace kernel {
 class Mouse : public Device {
 public:
     Type type() const override { return Type::MOUSE; }
+
+    struct Packet {
+        i8 wheel_delta;
+        i8 x_delta;
+        i8 y_delta;
+
+        bool left_button_held;
+        bool middle_button_held;
+        bool right_button_held;
+        bool button_4_held;
+        bool button_5_held;
+    };
 };
 }
