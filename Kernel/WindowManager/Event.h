b9a466106db5348458945b479e8ba30643e48267
8infy <99danilt@gmail.com>
2020-09-09 20:33:23 +0300

    [Kernel/WM] Added the Event structure, removed other types. Keyboard/Mouse events are gonna be added later as part of userland kernel API, the Event.h structures are supposed to be used for retrieving environment events via syscalls and avoid any c++ specific abi for portability.

--- Kernel/WindowManager/Event.h
+++ Kernel/WindowManager/Event.h
@@ -1,24 +1,40 @@
 #pragma once
 
-class Event {
-public:
+#include "VirtualKey.h"
+
+enum class ScrollDirection : unsigned char {
+    HORIZONTAL = 0,
+    VERTICAL   = 1
+};
+
+struct Event {
+    union {
+        struct {
+            size_t x;
+            size_t y;
+        } mouse_move;
+
+        struct {
+            ScrollDirection direction;
+            signed char delta;
+        } mouse_scroll;
+
+        struct {
+            VK      vkey;
+            VKState state;
+        } vk_state;
+    };
+
+    using MouseMove     = decltype(mouse_move);
+    using MouseScroll   = decltype(mouse_scroll);
+    using VKStateChange = decltype(vk_state);
+
     enum class Type : unsigned char {
-        MouseMove,
-        ButtonPress,
-        ButtonRelease,
-        MouseScroll,
-        KeyPress,
-        KeyRelease,
-        WindowResize,
-        WindowMove,
-    };
-
-    enum class Category : unsigned char {
-        MouseEvent,
-        KeyboardEvent,
-        WindowEvent,
-    };
-
-    virtual Type     type() const     = 0;
-    virtual Category category() const = 0;
+        MOUSE_MOVE,
+        BUTTON_STATE,
+        MOUSE_SCROLL,
+        KEY_STATE,
+        WINDOW_RESIZE,
+        WINDOW_MOVE,
+    } type;
 };
