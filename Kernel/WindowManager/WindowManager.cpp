31499d7c9fc4303798fd2e23c9f75db3fe1361d1
8infy <99danilt@gmail.com>
2020-09-29 20:02:04 +0300

    [Kernel/WM] WindowManager now owns the desktop window.

--- Kernel/WindowManager/WindowManager.cpp
+++ Kernel/WindowManager/WindowManager.cpp
@@ -10,6 +10,7 @@ WindowManager WindowManager::s_instance;
 void WindowManager::initialize()
 {
     Screen::initialize(VideoDevice::the());
+    the().m_desktop_window = Window::create_desktop(*Thread::current(), Screen::the().rect());
     Process::create_supervisor(&Compositor::run);
 }
 }
--- Kernel/WindowManager/WindowManager.h
+++ Kernel/WindowManager/WindowManager.h
@@ -9,7 +9,18 @@ class WindowManager {
 public:
     static void initialize();
 
+    // TODO: add locking here
+    void                                add_window(const RefPtr<Window>& window) { m_windows.emplace(window); }
+    const DynamicArray<RefPtr<Window>>& windows() { return m_windows; }
+
+    static WindowManager& the() { return s_instance; }
+
+    RefPtr<Window>& desktop() { return m_desktop_window; }
+
 private:
+    RefPtr<Window> m_desktop_window;
+
+    // TODO: this needs to be a linked list
     DynamicArray<RefPtr<Window>> m_windows;
 
     static WindowManager s_instance;
