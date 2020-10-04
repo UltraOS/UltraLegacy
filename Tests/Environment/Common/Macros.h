ab80609ad7fa7030431aa6211d430d05be7f41f2
8infy <99danilt@gmail.com>
2020-10-04 16:42:29 +0300

    [Kernel/Tests] Added missing KB/MB/GB macros.

--- Tests/Environment/Common/Macros.h
+++ Tests/Environment/Common/Macros.h
@@ -3,6 +3,10 @@
 #define ASSERT(x)
 #define hang()
 
+#define KB (1024ull)
+#define MB (1024ull * KB)
+#define GB (1024ull * MB)
+
 // Koenig lookup workaround
 #include "Utilities.h"
 #define move    ::kernel::move
