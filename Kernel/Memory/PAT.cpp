e55c4aa8d8b55e105bb523dfe025b993d3e042c5
8infy <99danilt@gmail.com>
2020-10-22 20:08:41 +0300

    [Kernel/Memory] Added PAT for manipulating the Page Attribute Table.
    This enables us to set up stuff like write combine caching
    on video memory, which makes writes to video memory many
    times faster on real hardware.

---
+++ Kernel/Memory/PAT.cpp
@@ -0,0 +1,49 @@
+#include "PAT.h"
+#include "Common/Logger.h"
+
+namespace kernel {
+
+PAT PAT::s_instance;
+
+PAT::PAT()
+{
+    static constexpr u32 pat_support_bit = SET_BIT(16);
+
+    m_is_supported = CPU::ID(0x01).d & pat_support_bit;
+
+    if (m_is_supported)
+        m_state = CPU::MSR::read(msr_index);
+}
+
+void PAT::set_entry(size_t index, MemoryType type)
+{
+    ASSERT(index > 3 && index < 8);
+
+    if (!m_is_supported) {
+        warning() << "PAT: cannot set memory type, feature unsupported.";
+        return;
+    }
+
+    LockGuard lock_guard(m_lock);
+
+    // Trying not to break strict aliasing :)
+    u8 msr_as_byte_array[sizeof(m_state.upper)] {};
+    copy_memory(&m_state.upper, msr_as_byte_array, sizeof(m_state.upper));
+
+    msr_as_byte_array[index - 4] = static_cast<u8>(type);
+
+    copy_memory(msr_as_byte_array, &m_state.upper, sizeof(m_state.upper));
+
+    m_state.write(msr_index);
+}
+
+void PAT::synchronize()
+{
+    if (!m_is_supported)
+        return;
+
+    LockGuard lock_guard(m_lock);
+
+    m_state.write(msr_index);
+}
+}
---
+++ Kernel/Memory/PAT.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "Common/Lock.h"
+#include "Common/Macros.h"
+#include "Common/Types.h"
+
+namespace kernel {
+
+class PAT {
+    MAKE_SINGLETON(PAT);
+
+public:
+    static constexpr u32 msr_index = 0x277;
+
+    enum class MemoryType : u8 {
+        UNCACHEABLE     = 0x00,
+        WRITE_COMBINING = 0x01,
+        WRITE_THROUGH   = 0x04,
+        WRITE_PROTECTED = 0x05,
+        WRITE_BACK      = 0x06,
+        UNCACHED        = 0x07
+    };
+
+    static PAT& the() { return s_instance; }
+
+    void set_entry(size_t index, MemoryType);
+    void synchronize();
+
+private:
+    InterruptSafeSpinLock m_lock;
+
+    CPU::MSR m_state;
+    bool     m_is_supported { false };
+
+    static PAT s_instance;
+};
+}
