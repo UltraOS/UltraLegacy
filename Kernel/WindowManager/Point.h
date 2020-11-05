34ab19ab7ae2d3e13bae1e1e649746154a61b80d
8infy <99danilt@gmail.com>
2020-11-05 20:13:41 +0300

    [Kernel/WM] Made Point constexpr.

--- Kernel/WindowManager/Point.h
+++ Kernel/WindowManager/Point.h
@@ -8,29 +8,42 @@ namespace kernel {
 template <typename T>
 class BasicPoint : public Pair<T, T> {
 public:
-    BasicPoint() = default;
-    BasicPoint(T x, T y)
+    constexpr BasicPoint() = default;
+    constexpr BasicPoint(T x, T y)
         : Pair<T, T>(x, y)
     {
     }
 
-    bool operator==(const BasicPoint& other) const { return (x() == other.x()) && (y() == other.y()); }
+    constexpr bool operator==(const BasicPoint& other) const { return (x() == other.x()) && (y() == other.y()); }
 
-    bool operator!=(const BasicPoint& other) const { return !(*this == other); }
+    constexpr bool operator!=(const BasicPoint& other) const { return !(*this == other); }
 
-    void move_by(T x, T y)
+    constexpr void move_by(T x, T y)
     {
         this->first() += x;
         this->second() += y;
     }
 
-    BasicPoint operator-() const
+    constexpr void move_by(BasicPoint other)
+    {
+        this->first() += other.first();
+        this->second() += other.second();
+    }
+
+    constexpr BasicPoint moved_by(BasicPoint other)
+    {
+        BasicPoint new_point = *this;
+        new_point.move_by(other);
+        return new_point;
+    }
+
+    constexpr BasicPoint operator-() const
     {
         return BasicPoint(-this->first(), -this->second());
     }
 
-    T x() const { return this->first(); }
-    T y() const { return this->second(); }
+    constexpr T x() const { return this->first(); }
+    constexpr T y() const { return this->second(); }
 };
 
 using Point = BasicPoint<ssize_t>;
