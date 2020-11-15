6e1a63b23c2eac2ff513987bd6e63fa0686c6501
8infy <99danilt@gmail.com>
2020-11-15 21:16:59 +0300

    [Kernel/Common] Implemented color_flip for RedBlackTree.

--- Kernel/Common/RedBlackTree.h
+++ Kernel/Common/RedBlackTree.h
@@ -12,7 +12,7 @@ public:
     template <typename... Args>
     void add(Args&&... args)
     {
-        auto* new_node = new Node { Node::Color::RED, nullptr, nullptr, nullptr, T(forward<Args>(args)...) };
+        auto* new_node = new Node(forward<Args>(args)...);
 
         if (m_root == nullptr) {
             m_root = new_node;
@@ -59,22 +59,102 @@ public:
 
 private:
     struct Node {
+        template <typename... Args>
+        Node(Args&&... args)
+                : value(forward<Args>(args)...)
+        {
+        }
+
+        Node* parent { nullptr };
+        Node* left { nullptr };
+        Node* right { nullptr };
+
         enum class Color : u8 {
             RED = 0,
             BLACK = 1
-        } color;
-
-        Node* parent;
-        Node* left;
-        Node* right;
+        } color { Color::RED };
 
         T value;
+
+        bool is_left_child() const
+        {
+            if (!parent || (parent->left == this))
+                return true;
+
+            return false;
+        }
+
+        bool is_right_child() const { return !is_left_child(); }
+
+        bool is_black() const { return color == Color::BLACK; }
+        bool is_red() const { return color == Color::RED; }
+
+        Node* grandparent() const
+        {
+            if (!parent)
+                return nullptr;
+
+            return parent->parent;
+        }
+
+        Node* aunt() const
+        {
+            auto* grandparent = this->grandparent();
+
+            if (!grandparent)
+                return nullptr;
+
+            if (parent->is_left_child())
+                return grandparent->right;
+
+            return grandparent->left;
+        }
+
+        bool is_aunt_black() const
+        {
+            auto* aunt = this->aunt();
+
+            if (!aunt || aunt->is_black())
+                return true;
+
+            return false;
+        }
+
+        bool is_aunt_red() const
+        {
+            return !is_aunt_black();
+        }
+
     }* m_root { nullptr };
 
-    void fix_violations_if_needed(Node* new_node)
+    void fix_violations_if_needed(Node* node)
     {
-        (void) new_node;
+        while (node != m_root) {
+            if (node->parent->is_black() || node->is_black())
+                return;
 
+            if (node->is_aunt_red())
+                color_filp(node);
+            else
+                rotate(node);
+
+            node = node->parent;
+        }
+    }
+
+    void color_filp(Node* violating_node)
+    {
+        auto* grandparent = violating_node->grandparent();
+
+        if (grandparent != m_root)
+            grandparent->color = Node::Color::RED;
+
+        grandparent->left->color = Node::Color::BLACK;
+        grandparent->right->color = Node::Color::BLACK;
+    }
+
+    void rotate(Node* violating_node)
+    {
         // TODO: implement
     }
 };
