c03b9e0129d59308f9b09907254def56faef1782
8infy <99danilt@gmail.com>
2020-12-18 21:23:34 +0300

    [Kernel/Memory] Added BootAllocator.
    BootAllocator is a new type of allocator in the system. It allows
    anyone to allocate an arbitrary amount of physical pages before the
    main MemoryManager is initialized. Right now it's only used to reserve
    the kernel image from the physical memory map as well as allocate space
    for the initial kernel heap block.

---
+++ Kernel/Memory/BootAllocator.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include "Common/Types.h"
+#include "Common/Macros.h"
+#include "Common/String.h"
+#include "MemoryMap.h"
+
+namespace kernel {
+
+class BootAllocator {
+    MAKE_SINGLETON(BootAllocator, MemoryMap);
+
+public:
+    // each allocation must be given a tag for easier tracking
+    enum class Tag {
+        GENERIC,
+        KERNEL_IMAGE,
+        KERNEL_MODULE,
+        HEAP_BLOCK,
+    };
+
+    static constexpr StringView tag_to_string(Tag tag)
+    {
+        switch (tag)
+        {
+        case Tag::GENERIC:
+            return "generic"_sv;
+        case Tag::KERNEL_IMAGE:
+            return "kernel image"_sv;
+        case Tag::KERNEL_MODULE:
+            return "kernel module"_sv;
+        case Tag::HEAP_BLOCK:
+            return "heap block"_sv;
+        default:
+            return "<unknown>"_sv;
+        }
+    }
+
+    static void initialize(MemoryMap);
+    static BootAllocator& the();
+
+    Address64 reserve_at(Address64, size_t page_count, Tag = Tag::GENERIC);
+    Address64 reserve_contiguous(size_t page_count, Address64 lower_limit, Address64 upper_limit, Tag = Tag::GENERIC);
+
+    MemoryMap release();
+
+private:
+    constexpr MemoryMap::PhysicalRange::Type native_type_from_tag(Tag tag) const
+    {
+        using NativeType = MemoryMap::PhysicalRange::Type;
+
+        switch (tag) {
+            case Tag::GENERIC:
+                return NativeType::GENERIC_BOOTALLOC_RESERVED;
+            case Tag::KERNEL_IMAGE:
+                return NativeType::KERNEL_IMAGE;
+            case Tag::KERNEL_MODULE:
+                return NativeType::KERNEL_MODULE;
+            case Tag::HEAP_BLOCK:
+                return NativeType::INITIAL_HEAP_BLOCK;
+            default:
+                ASSERT_NEVER_REACHED();
+        }
+    }
+
+private:
+    bool m_did_release { false };
+    MemoryMap m_memory_map;
+};
+
+}
