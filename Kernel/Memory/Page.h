18bfb9bc0f0864af91a37600497c419b08527fef
8infy <99danilt@gmail.com>
2020-12-23 12:29:41 +0300

    [Kernel/Memory] Added Page::round_{up/down}_huge & a few new things.

--- Kernel/Memory/Page.h
+++ Kernel/Memory/Page.h
@@ -8,33 +8,60 @@ namespace kernel {
 
 class Page {
 public:
+    static constexpr size_t size = 4096;
+
 #ifdef ULTRA_32
     static constexpr size_t huge_size = 4 * MB;
-    static constexpr Address alignment_mask = 0xFFFFF000;
+    static constexpr ptr_t alignment_mask = 0xFFFFF000;
+    static constexpr ptr_t huge_alignment_mask = 0xFFFFFFFF - huge_size + 1;
 #elif defined(ULTRA_64)
     static constexpr size_t huge_size = 2 * MB;
-    static constexpr Address alignment_mask = 0xFFFFFFFFFFFFF000;
+    static constexpr ptr_t alignment_mask = 0xFFFFFFFFFFFFF000;
+    static constexpr ptr_t huge_alignment_mask = 0xFFFFFFFFFFFFFFFF - huge_size + 1;
 #endif
-
-    static constexpr size_t size = 4096;
 
     explicit Page(Address physical_address);
 
     Address address() const;
 
-    static constexpr bool is_aligned(Address address) { return (address % size) == 0; }
+    template <ptr_t AlignmentMask, size_t PageSize>
+    static constexpr size_t round_up_custom(size_t size)
+    {
+        if (size == 0)
+            return PageSize;
+
+        return size & ~AlignmentMask ? (size + PageSize) & AlignmentMask : size;
+    }
+
+    template <ptr_t AlignmentMask>
+    static constexpr size_t round_down_custom(size_t size)
+    {
+        return size & ~AlignmentMask ? size & AlignmentMask : size;
+    }
 
     static constexpr size_t round_up(size_t size)
     {
-        if (size == 0)
-            return Page::size;
+        return round_up_custom<Page::alignment_mask, Page::size>(size);
+    }
 
-        return size & ~Page::alignment_mask ? (size + Page::size) & Page::alignment_mask : size;
+    static constexpr size_t round_up_huge(size_t size)
+    {
+        return round_up_custom<Page::huge_alignment_mask, Page::huge_size>(size);
     }
 
     static constexpr size_t round_down(size_t size)
     {
-        return size & ~Page::alignment_mask ? size & Page::alignment_mask : size;
+        return round_down_custom<Page::alignment_mask>(size);
+    }
+
+    static constexpr size_t round_down_huge(size_t size)
+    {
+        return round_down_custom<Page::huge_alignment_mask>(size);
+    }
+
+    static constexpr bool is_aligned(Address address)
+    {
+        return (address % size) == 0;
     }
 
     static constexpr bool is_huge_aligned(Address address)
