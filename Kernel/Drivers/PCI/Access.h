e59690e5aa4325c795f0362dc2b782cfed8f59f3
8infy <99danilt@gmail.com>
2021-02-21 00:09:09 +0300

    [Kernel/Drivers] Fixed a few bus in PCI::Access::read16/32

--- Kernel/Drivers/PCI/Access.h
+++ Kernel/Drivers/PCI/Access.h
@@ -14,36 +14,46 @@ public:
     virtual u32 read32(PCI::Location, u32 offset) = 0;
     virtual void write32(PCI::Location, u32 offset, u32 value) = 0;
 
+    static constexpr u32 aligned_mask = 0b11111100;
+
     u16 read16(PCI::Location location, u32 offset)
     {
-        auto res = read32(location, offset & 0xFC);
+        auto res = read32(location, offset & aligned_mask);
         res >>= (offset & 2) * 8;
         return res & 0xFFFF;
     }
 
     u8 read8(PCI::Location location, u32 offset)
     {
-        auto res = read32(location, offset & 0xFC);
+        auto res = read32(location, offset & aligned_mask);
         res >>= (offset & 3) * 8;
         return res & 0xFF;
     }
 
     void write16(PCI::Location location, u32 offset, u16 value)
     {
-        auto res = read32(location, offset & 0xFC);
+        auto res = read32(location, offset & aligned_mask);
 
-        res |= value << (offset & 2);
+        bool upper = offset & 2;
 
-        write32(location, offset & 0xFC, res);
+        if (upper)
+            res = (value << 16) | (res & 0xFFFF);
+        else
+            res = (res & 0xFFFF0000) | value;
+
+        write32(location, offset & aligned_mask, res);
     }
 
     void write8(PCI::Location location, u32 offset, u8 value)
     {
-        auto res = read32(location, offset & 0xFC);
+        auto res = read32(location, offset & aligned_mask);
 
-        res |= value << (offset & 3);
+        u8 res_as_array[4];
+        copy_memory(&res, res_as_array, sizeof(res));
+        res_as_array[offset & 0b11] = value;
+        copy_memory(res_as_array, &res, sizeof(res));
 
-        write32(location, offset & 0xFC, res);
+        write32(location, offset & aligned_mask, res);
     }
 
     virtual DynamicArray<PCI::DeviceInfo> list_all_devices() = 0;
