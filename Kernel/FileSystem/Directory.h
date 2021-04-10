4701e44f51335a47ee8d56d2b616a8eaa5f1bfb2
8infy <99danilt@gmail.com>
2021-04-10 20:19:32 +0300

    [Kernel/FS] Added Directory.

---
+++ Kernel/FileSystem/Directory.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "File.h"
+
+namespace kernel {
+
+class FileSystem;
+
+class Directory
+{
+public:
+    Directory(FileSystem& fs)
+        : m_fs(fs)
+    {
+    }
+
+    struct Entry {
+        char name[File::max_name_length + 1];
+        char small_name[File::max_name_length + 1];
+        File::Attributes attributes;
+        u64 size;
+
+        StringView name_view() const { return StringView(name); }
+        StringView small_name_view() const { return StringView(small_name); }
+        bool empty() const { return name[0] == '\0'; }
+    };
+
+    virtual bool has_next() = 0;
+    virtual Entry next() = 0;
+    virtual Entry previous() = 0;
+
+    FileSystem& fs() { return m_fs; }
+
+    ~Directory() = default;
+
+private:
+    FileSystem& m_fs;
+};
+
+
+}
