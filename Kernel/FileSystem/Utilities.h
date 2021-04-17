4f79853440108e23731dcf94ccffeaf1851fc2cf
8infy <99danilt@gmail.com>
2021-04-17 21:31:04 +0300

    [Kernel/FS] Added IterablePath, adjusted path validation. New tests.
    We now consider paths like //hello/////world// valid.

--- Kernel/FileSystem/Utilities.h
+++ Kernel/FileSystem/Utilities.h
@@ -21,9 +21,6 @@ inline bool is_valid_path(StringView path)
         char rhs = i < path.size() - 1 ? path[i + 1] : valid_decoy;
         char cur = path[i];
 
-        if (cur == '/' && (lhs == '/' || rhs == '/'))
-            return false;
-
         if (cur == '.' && (lhs == '.' && rhs == '.'))
             return false;
     }
@@ -64,5 +61,72 @@ inline Optional<Pair<StringView, StringView>> split_prefix_and_path(StringView p
 
     return prefix_to_path;
 }
+
+inline StringView next_path_node(const char* current, const char* end)
+{
+    size_t length = 0;
+
+    while (current != end && current[0] == '/')
+            current++;
+
+    while ((current + length) != end && current[length] != '/')
+        length++;
+
+    return { current, length };
+}
+
+class IterablePath {
+public:
+    IterablePath(StringView path)
+        : m_current(path.begin())
+        , m_current_end(path.begin())
+        , m_end(path.end())
+    {
+        next();
+    }
+
+    StringView operator*() const
+    {
+        return { m_current, m_current_end };
+    }
+
+    bool operator==(const IterablePath& other) const
+    {
+        return m_current == other.m_current;
+    }
+
+    bool operator!=(const IterablePath& other) const
+    {
+        return !this->operator==(other);
+    }
+
+    IterablePath& operator++()
+    {
+        next();
+        return *this;
+    }
+
+    IterablePath begin() { return IterablePath({ m_current, m_end }); }
+    IterablePath end() { return IterablePath({ m_end, m_end }); }
+
+private:
+    void next()
+    {
+        if (m_current_end == m_end)
+            m_current = m_end;
+
+        if (m_current == m_end)
+            return;
+
+        auto node = next_path_node(m_current_end, m_end);
+        m_current = node.begin();
+        m_current_end = node.end();
+    }
+
+private:
+    const char* m_current;
+    const char* m_current_end;
+    const char* m_end;
+};
 
 }
--- Tests/Source/TestFileSystem.cpp
+++ Tests/Source/TestFileSystem.cpp
@@ -1,4 +1,5 @@
 #include "TestRunner.h"
+#include <array>
 
 #include "FileSystem/Utilities.h"
 
@@ -55,12 +56,60 @@ TEST(SplitPrefxAndPath) {
     Assert::that(val_7.first()).is_equal("ASD");
     Assert::that(val_7.second()).is_equal("/::hello");
 
-    Assert::that(res_8.has_value()).is_false();
-    Assert::that(res_9.has_value()).is_false();
+    Assert::that(res_8.has_value()).is_true();
+    auto& val_8 = res_8.value();
+    Assert::that(val_8.first().empty()).is_true();
+    Assert::that(val_8.second()).is_equal("///ASD///hello world");
+
+    Assert::that(res_9.has_value()).is_true();
+    auto& val_9 = res_9.value();
+    Assert::that(val_9.first().empty()).is_true();
+    Assert::that(val_9.second()).is_equal("/:://::test");
+
     Assert::that(res_10.has_value()).is_false();
 
     Assert::that(res_11.has_value()).is_true();
     auto& val_11 = res_11.value();
     Assert::that(val_11.first().empty()).is_true();
     Assert::that(val_11.second()).is_equal("/hello/./world/../test/../something/..");
+}
+
+TEST(NextNode) {
+    using namespace kernel;
+
+    StringView path_1 = "/hello/world/./../A/B/C"_sv;
+
+    const std::array<std::string, 7> nodes_1 = {
+        "hello", "world", ".", "..", "A", "B", "C"
+    };
+
+    size_t index_1 = 0;
+    for (auto c : IterablePath(path_1)) {
+        Assert::that(index_1).is_less_than(7);
+        Assert::that(nodes_1[index_1++]).is_equal(std::string(c.begin(), c.end()));
+    }
+
+    StringView path_2 = "/test/1/"_sv;
+
+    const std::array<std::string, 2> nodes_2 = {
+        "test", "1"
+    };
+
+    size_t index_2 = 0;
+    for (auto c : IterablePath(path_2)) {
+        Assert::that(index_2).is_less_than(2);
+        Assert::that(nodes_2[index_2++]).is_equal(std::string(c.begin(), c.end()));
+    }
+
+    StringView path_3 = "///test////1//h/123//////"_sv;
+
+    const std::array<std::string, 4> nodes_3 = {
+        "test", "1", "h", "123"
+    };
+
+    size_t index_3 = 0;
+    for (auto c : IterablePath(path_3)) {
+        Assert::that(index_3).is_less_than(4);
+        Assert::that(nodes_3[index_3++]).is_equal(std::string(c.begin(), c.end()));
+    }
 }
