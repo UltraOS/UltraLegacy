a78cf98ca360ce5eba99dc1878a07cc74d69fce3
8infy <99danilt@gmail.com>
2021-05-05 23:46:02 +0700

    [Scripts] run_clang_format.sh MacOS support.

--- Scripts/run_clang_format.sh
+++ Scripts/run_clang_format.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  realpath() {
+      [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+  }
+fi
+
 on_error()
 {
     echo "Failed to run clang format"
@@ -10,4 +16,4 @@ true_path="$(dirname "$(realpath "$0")")"
 root_path=$true_path/..
 path_to_kernel=$root_path/Kernel
 
-find $path_to_kernel -regex ".*\.\(cpp\|h\)" -exec clang-format -style=file -i {} \; || on_error
+find $path_to_kernel -type f \( -name "*cpp" -o -name "*h" \) -exec clang-format -style=file -i {} \; || on_error
