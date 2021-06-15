868f3c331c5eeb2a8c2e0896606c6b3cd42b61d5
8infy <99danilt@gmail.com>
2021-06-15 23:41:15 +0300

    [Kernel/Memory] Began work on SharedVirtualRegion.

---
+++ Kernel/Memory/SharedVirtualRegion.cpp
@@ -0,0 +1,49 @@
+#include "SharedVirtualRegion.h"
+
+namespace kernel {
+
+SharedVirtualRegion::SharedVirtualRegion(Range range, Properties properties, StringView name)
+    : VirtualRegion(range, properties, name)
+{
+    m_shared_block = new SharedBlock;
+    m_shared_block->ref_count = 1;
+}
+
+SharedVirtualRegion::SharedVirtualRegion(Range range, Properties properties, const SharedVirtualRegion& to_clone)
+    : VirtualRegion(range, properties, to_clone.name().to_view())
+    , m_shared_block(to_clone.m_shared_block)
+{
+    m_shared_block->ref_count++;
+}
+
+SharedVirtualRegion* SharedVirtualRegion::clone(Range virtual_range, IsSupervisor is_supervisor)
+{
+    Properties props {};
+    if (is_supervisor == IsSupervisor::YES)
+        props += Properties::SUPERVISOR;
+    props += Properties::SHARED;
+
+    return new SharedVirtualRegion(virtual_range, props, *this);
+}
+
+void SharedVirtualRegion::store_page(Page page, Address virtual_address)
+{
+    auto offset_from_base = virtual_address - this->virtual_range().begin();
+    offset_from_base /= Page::size;
+
+    m_shared_block->pages.expand_to(offset_from_base + 1);
+    m_shared_block->pages[offset_from_base] = page;
+}
+
+Page SharedVirtualRegion::page_at(Address virtual_address)
+{
+    auto offset_from_base = virtual_address - this->virtual_range().begin();
+    offset_from_base /= Page::size;
+
+    if (m_shared_block->pages.size() <= offset_from_base)
+        return {};
+
+    return m_shared_block->pages[offset_from_base];
+}
+
+}
---
+++ Kernel/Memory/SharedVirtualRegion.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "VirtualRegion.h"
+#include "Common/DynamicArray.h"
+#include "Page.h"
+
+namespace kernel {
+
+class SharedVirtualRegion : public VirtualRegion {
+public:
+    SharedVirtualRegion(Range range, Properties properties, StringView name);
+
+private:
+    SharedVirtualRegion(Range range, Properties properties, const SharedVirtualRegion& to_clone);
+    SharedVirtualRegion* clone(Range virtual_range, IsSupervisor);
+
+    struct SharedBlock {
+        InterruptSafeSpinLock modification_lock;
+        DynamicArray<Page> pages;
+        Atomic<size_t> ref_count;
+    }* m_shared_block;
+
+    friend class MemoryManager;
+
+    InterruptSafeSpinLock& lock() { return m_shared_block->modification_lock; }
+    size_t decref() { return --m_shared_block->ref_count; }
+    SharedBlock& shared_block() { return *m_shared_block; }
+
+    void store_page(Page page, Address virtual_address);
+    Page page_at(Address virtual_address);
+    DynamicArray<Page>& owned_pages() { return m_shared_block->pages; }
+};
+
+}
