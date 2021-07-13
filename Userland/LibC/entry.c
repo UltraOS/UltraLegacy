eb462cc581d7e5f14c29832847e89f7d6f3647b2
8infy <99danilt@gmail.com>
2021-07-13 23:29:30 +0300

    [Userland/LibC] Now passing envp to main + misc fixes.

--- Userland/LibC/entry.c
+++ Userland/LibC/entry.c
@@ -1,6 +1,6 @@
 #include "stdlib.h"
 
-int main(int argc, char** argv);
+int main(int argc, char** argv, char** envp);
 
 void _init();
 void stdio_init();
@@ -14,8 +14,9 @@ void libc_entrypoint(void* sysv_stack)
 
     int argc = *(int*)sysv_stack;
     char** argv = (char**)((char*)sysv_stack + sizeof(size_t));
+    char** envp = (char**)((char*)sysv_stack + (sizeof(size_t) * (argc + 2))); // 2 to skip over argc and argv[argc]
 
-    int ret = main(argc, argv);
+    int ret = main(argc, argv, envp);
 
     exit(ret);
 }
