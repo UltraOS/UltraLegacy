58515cb49f7ab1e8dd2cc0e354cf7553b17b3332
8infy <99danilt@gmail.com>
2021-07-27 20:00:34 +0300

    [Kernel/Common] Expanded Span.

--- Kernel/Common/Span.h
+++ Kernel/Common/Span.h
@@ -15,13 +15,62 @@ public:
     {
     }
 
+    template <size_t N>
+    Span(T (&array)[N])
+        : m_data(array)
+        , m_size(N)
+    {
+    }
+
     size_t size() const { return m_size; }
 
     T* data() { return m_data; }
     const T* data() const { return m_data; }
 
+    T& at(size_t index)
+    {
+        ASSERT(index < m_size);
+        return m_data[index];
+    }
+
+    const T& at(size_t index) const
+    {
+        ASSERT(index < m_size);
+        return m_data[index];
+    }
+
+    T& operator[](size_t index) { return at(index); }
+    const T& operator[](size_t index) const { return at(index); }
+
+    T& back()
+    {
+        ASSERT(!empty());
+        return m_data[m_size - 1];
+    }
+
+    const T& back() const
+    {
+        ASSERT(!empty());
+        return m_data[m_size - 1];
+    }
+
+    T& front()
+    {
+        ASSERT(!empty());
+        return m_data[0];
+    }
+
+    const T& front() const
+    {
+        ASSERT(!empty());
+        return m_data[0];
+    }
+
     T* begin() { return m_data; }
+    const T* begin() const { return m_data; }
+
     T* end() { return m_data + m_size; }
+    const T* end() const { return m_data + m_size; }
 
     bool empty() { return m_size == 0; }
 
