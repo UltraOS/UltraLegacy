a33a09be0894fa26e0bbd3b2299a3a5e43af2d21
8infy <99danilt@gmail.com>
2021-08-12 23:40:07 +0300

    [Kernel/Common] Rewrote DynamicArray.
    Previously it was very buggy and all over the place.
    This patch aims to make it more readable and safe
    by removing unncessary code paths and functions,
    as well as simplifying overall design.

--- Kernel/Common/DynamicArray.h
+++ Kernel/Common/DynamicArray.h
@@ -22,18 +22,68 @@ public:
             emplace(item);
     }
 
-    DynamicArray(size_t initial_capacity) { reserve(initial_capacity); }
+    DynamicArray(size_t reserved_count)
+    {
+        reserve(reserved_count);
+    }
 
-    DynamicArray(const DynamicArray& other) { become(other); }
+    DynamicArray(const DynamicArray& arr)
+    {
+        if (arr.empty())
+            return;
 
-    DynamicArray(DynamicArray&& other) { become(move(other)); }
+        reserve(arr.size());
+
+        for (size_t i = 0; i < arr.size(); ++i)
+            new (&m_data[i]) T(arr[i]);
+
+        m_size = arr.size();
+    }
+
+    DynamicArray(DynamicArray&& other)
+    {
+        swap(m_data, other.m_data);
+        swap(m_size, other.m_size);
+        swap(m_capacity, other.m_capacity);
+    }
 
     DynamicArray& operator=(const DynamicArray& other)
     {
         if (this == &other)
             return *this;
 
-        become(other);
+        if (other.empty()) {
+            clear();
+            return *this;
+        }
+
+        // Since we have to allocate a new buffer anyway, we can simply
+        // destroy all the elements, and then copy other into the new buffer.
+        if (m_capacity < other.size()) {
+            clear();
+            reserve(other.size());
+
+            for (size_t i = 0; i < other.size(); ++i)
+                new (&m_data[i]) T(other[i]);
+
+            m_size = other.m_size;
+
+            return *this;
+        }
+
+        size_t count_to_replace = min(m_size, other.m_size);
+        size_t count_to_construct = other.m_size - count_to_replace;
+        size_t count_to_destroy = (other.m_size < m_size) ? m_size - other.m_size : 0;
+
+        size_t i = 0;
+        for (; i < count_to_replace; ++i)
+            m_data[i] = other[i];
+        for (; i < count_to_construct; ++i)
+            new (&m_data[i]) T(other[i]);
+        for (; i < count_to_destroy; ++i)
+            m_data[i].~T();
+
+        m_size = other.m_size;
 
         return *this;
     }
@@ -43,9 +93,92 @@ public:
         if (this == &other)
             return *this;
 
-        become(forward<DynamicArray>(other));
+        swap(m_data, other.m_data);
+        swap(m_size, other.m_size);
+        swap(m_capacity, other.m_capacity);
 
         return *this;
+    }
+
+    T& append(const T& item)
+    {
+        ensure_space();
+
+        return *new (m_data + m_size++) T(item);
+    }
+
+    T& append(T&& item)
+    {
+        ensure_space();
+
+        return *new (m_data + m_size++) T(move(item));
+    }
+
+    template <typename... Args>
+    T& emplace(Args&&... args)
+    {
+        ensure_space();
+
+        return *new (m_data + m_size++) T(forward<Args>(args)...);
+    }
+
+    [[nodiscard]] size_t size() const { return m_size; }
+    [[nodiscard]] bool empty() const { return m_size == 0; }
+    [[nodiscard]] size_t capacity() const { return m_capacity; }
+
+    T* begin() { return m_data; }
+    const T* begin() const { return m_data; }
+
+    T* end() { return m_data + m_size; }
+    const T* end() const { return m_data + m_size; }
+
+    T* data() { return m_data; }
+
+    T& at(size_t i)
+    {
+        ASSERT(i < m_size);
+        return m_data[i];
+    }
+
+    const T& at(size_t i) const
+    {
+        ASSERT(i < m_size);
+        return m_data[i];
+    }
+
+    T& operator[](size_t i) { return at(i); }
+    const T& operator[](size_t i) const { return at(i); }
+
+    T& first()
+    {
+        ASSERT(!empty());
+        return m_data[0];
+    }
+
+    T& last()
+    {
+        ASSERT(!empty());
+        return m_data[m_size - 1];
+    }
+
+    void reserve(size_t count)
+    {
+        if (!count || count < m_capacity)
+            return;
+
+        T* new_buffer = reinterpret_cast<T*>(new u8[sizeof(T) * count]);
+
+        for (size_t i = 0; i < m_size; ++i) {
+            auto& this_elem = at(i);
+
+            new (&new_buffer[i]) T(move(this_elem));
+            this_elem.~T();
+        }
+
+        m_capacity = count;
+
+        delete[] reinterpret_cast<u8*>(m_data);
+        m_data = new_buffer;
     }
 
     void expand_to(size_t count)
@@ -55,252 +188,66 @@ public:
 
         reserve(count);
 
-        if (is_trivially_constructible_v<T>)
-            zero_memory(address_of(m_size, m_data), (m_capacity - m_size) * sizeof(T));
-        else {
-            for (size_t i = m_size + !m_size - 1; i < m_capacity; ++i)
-                new (address_of(i, m_data)) T();
-        }
+        for (size_t i = 0; i < (count - m_size); ++i)
+            new (&m_data[m_size + i]) T();
 
         m_size = count;
-    }
-
-    void reserve(size_t count)
-    {
-        if (count <= capcity())
-            return;
-
-        grow_by(count - capcity());
-    }
-
-    [[nodiscard]] T& at(size_t index)
-    {
-        ASSERT(index < m_size);
-
-        T* elems = reinterpret_cast<T*>(m_data);
-
-        return elems[index];
-    }
-
-    [[nodiscard]] const T& at(size_t index) const
-    {
-        ASSERT(index < m_size);
-
-        T* elems = reinterpret_cast<T*>(m_data);
-
-        return elems[index];
-    }
-
-    [[nodiscard]] T& first()
-    {
-        ASSERT(m_size > 0);
-
-        return at(0);
-    }
-
-    [[nodiscard]] const T& first() const
-    {
-        ASSERT(m_size > 0);
-
-        return at(0);
-    }
-
-    [[nodiscard]] T& last()
-    {
-        ASSERT(m_size != 0);
-
-        return at(m_size - 1);
-    }
-
-    [[nodiscard]] const T& last() const
-    {
-        ASSERT(m_size != 0);
-
-        return at(m_size - 1);
-    }
-
-    [[nodiscard]] const T& operator[](size_t index) const { return at(index); }
-
-    [[nodiscard]] T& operator[](size_t index) { return at(index); }
-
-    [[nodiscard]] T* data() { return reinterpret_cast<T*>(m_data); }
-
-    [[nodiscard]] const T* data() const { return reinterpret_cast<T*>(m_data); }
-
-    T& append(T&& value)
-    {
-        grow_if_full();
-
-        new (address_of(m_size, m_data)) T(forward<T&&>(value));
-        ++m_size;
-
-        return last();
-    }
-
-    T& append(const T& value)
-    {
-        grow_if_full();
-
-        new (address_of(m_size, m_data)) T(value);
-        ++m_size;
-
-        return last();
-    }
-
-    template <typename... Args>
-    T& emplace(Args&&... args)
-    {
-        grow_if_full();
-
-        new (address_of(m_size, m_data)) T(forward<Args>(args)...);
-        ++m_size;
-
-        return last();
     }
 
     void erase(T* itr)
     {
         ASSERT(itr >= begin() && itr < end());
 
-        erase_at(itr - begin());
+        erase_at_unchecked(itr - begin());
     }
 
     void erase_at(size_t i)
     {
         ASSERT(i < m_size);
 
-        if constexpr (!is_trivially_destructible_v<T>)
-            at(i).~T();
-
-        // if it wasn't the last element we have to copy everything
-        if (i != m_size - 1)
-            move_elements(i + 1, i, m_size - i - 1);
-
-        --m_size;
+        erase_at_unchecked(i);
     }
-
-    size_t size() const { return m_size; }
-    bool empty() const { return size() == 0; }
-
-    size_t capcity() const { return m_capacity; }
-
-    ~DynamicArray() { destroy_data(); }
-
-    T* begin() { return data(); }
-
-    T* end() { return data() + m_size; }
-
-    const T* begin() const { return data(); }
-
-    const T* end() const { return data() + m_size; }
 
     void clear()
     {
-        destroy_all();
+        for (size_t i = 0; i < m_size; ++i)
+            m_data[i].~T();
+
         m_size = 0;
     }
 
-private:
-    void move_elements(size_t from, size_t to, size_t count)
+    ~DynamicArray()
     {
-        if constexpr (is_trivially_copyable_v<T>)
-            move_memory(address_of(from, m_data), address_of(to, m_data), count * sizeof(T));
-        else {
-            // TODO: this only works for moving elements backwars, rewrite to support inserts at random locations
-            for (size_t i = 0; i < count; ++i) {
-                auto index = from + i;
-
-                new (address_of(index - 1, m_data)) T(move(at(index)));
-
-                if constexpr (!is_trivially_destructible_v<T>)
-                    at(index).~T();
-            }
-        }
-    }
-
-    void become(const DynamicArray& other)
-    {
-        if (other.empty()) {
-            clear();
-            return;
-        }
-
-        destroy_data();
-        grow_by(other.size());
-
-        copy_external_elements(other.m_data, m_data, other.size());
-
-        m_size = other.m_size;
-    }
-
-    void become(DynamicArray&& other)
-    {
-        swap(m_data, other.m_data);
-        swap(m_size, other.m_size);
-        swap(m_capacity, other.m_capacity);
-    }
-
-    void copy_external_elements(u8* from, u8* to, size_t count)
-    {
-        if constexpr (is_trivially_copyable_v<T>)
-            copy_memory(from, to, count * sizeof(T));
-        else {
-            for (size_t i = 0; i < count; ++i)
-                new (address_of(i, to)) T(*reinterpret_cast<T*>(address_of(i, from)));
-        }
-    }
-
-    void grow_if_full()
-    {
-        if (m_size < m_capacity)
-            return;
-
-        grow_by(max(static_cast<decltype(m_capacity)>(1), m_capacity / 2));
-    }
-
-    u8* address_of(size_t index, u8* begin) { return begin + (index * sizeof(T)); }
-
-    void destroy_data()
-    {
-        destroy_all();
-
-        delete[] m_data;
-    }
-
-    void grow_by(size_t element_count)
-    {
-        if (element_count == 0)
-            return;
-
-        auto new_capacity = element_count + m_capacity;
-
-        auto* new_data = new u8[new_capacity * sizeof(T)];
-        ASSERT((reinterpret_cast<decltype(alignof(T))>(new_data) % alignof(T)) == 0);
-
-        if constexpr (is_trivially_copyable_v<T>)
-            copy_memory(m_data, new_data, m_size * sizeof(T));
-        else {
-            for (size_t i = 0; i < m_size; ++i)
-                new (address_of(i, new_data)) T(move(at(i)));
-        }
-
-        destroy_data();
-
-        m_data = new_data;
-        m_capacity = new_capacity;
-    }
-
-    void destroy_all()
-    {
-        if constexpr (!is_trivially_destructible_v<T>) {
-            for (size_t i = 0; i < m_size; ++i)
-                at(i).~T();
-        }
+        clear();
+        delete[] reinterpret_cast<u8*>(m_data);
     }
 
 private:
+    void ensure_space()
+    {
+        if (m_size != m_capacity)
+            return;
+
+        reserve(m_capacity + max<size_t>(m_capacity / 2, 2));
+    }
+
+    void erase_at_unchecked(size_t i)
+    {
+        // Move each element one index before current
+        for (; i < m_size - 1; ++i)
+            m_data[i] = move(m_data[i + 1]);
+
+        // Last element is now in moved-from state, just destroy it
+        m_data[m_size - 1].~T();
+
+        // and remove it from the array
+        m_size--;
+    }
+
+private:
+    T* m_data { nullptr };
     size_t m_size { 0 };
     size_t m_capacity { 0 };
-    u8* m_data { nullptr };
 };
+
 }
