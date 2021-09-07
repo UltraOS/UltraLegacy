9fa859fb38f8317f3ed91aa4d90f39734c7046a3
8infy <99danilt@gmail.com>
2021-09-07 23:38:18 +0300

    [Kernel/Multitasking] Added SharedMutex.

--- Kernel/Multitasking/Mutex.cpp
+++ Kernel/Multitasking/Mutex.cpp
@@ -47,4 +47,90 @@ void Mutex::unlock()
     blocker.unblock();
 }
 
+void SharedMutex::exclusive_lock()
+{
+    for (;;) {
+        bool interrupt_state = false;
+        m_state_access_lock.lock(interrupt_state, __FILE__, __LINE__, CPU::current_id());
+
+        auto* current_thread = Thread::current();
+
+        // Threads shouldn't be able to die when acquiring/owning a mutex
+        ASSERT(current_thread->is_invulnerable());
+
+        if (m_shared_refcount == free_mode) {
+            m_shared_refcount = exclusive_mode;
+            m_state_access_lock.unlock(interrupt_state);
+            return;
+        }
+
+        MutexBlocker blocker(*current_thread);
+        m_exclusive_waiters.insert_back(blocker);
+        m_state_access_lock.unlock(interrupt_state);
+
+        auto res = blocker.block();
+        ASSERT(res == Blocker::Result::UNBLOCKED);
+    }
+}
+
+void SharedMutex::exclusive_unlock()
+{
+    LOCK_GUARD(m_state_access_lock);
+
+    ASSERT(m_shared_refcount == exclusive_mode);
+    m_shared_refcount = free_mode;
+
+    bool woke_one = false;
+
+    while (!m_shared_waiters.empty()) {
+        woke_one = true;
+        auto& waiter = m_shared_waiters.pop_front();
+        waiter.unblock();
+    }
+
+    if (woke_one)
+        return;
+
+    if (!m_exclusive_waiters.empty())
+        m_exclusive_waiters.pop_front().unblock();
+}
+
+void SharedMutex::shared_lock()
+{
+    for (;;) {
+        bool interrupt_state = false;
+        m_state_access_lock.lock(interrupt_state, __FILE__, __LINE__, CPU::current_id());
+
+        auto* current_thread = Thread::current();
+
+        // Threads shouldn't be able to die when acquiring/owning a mutex
+        ASSERT(current_thread->is_invulnerable());
+
+        if (m_shared_refcount >= free_mode) {
+            m_shared_refcount++;
+            m_state_access_lock.unlock(interrupt_state);
+            return;
+        }
+
+        MutexBlocker blocker(*current_thread);
+        m_shared_waiters.insert_back(blocker);
+        m_state_access_lock.unlock(interrupt_state);
+
+        auto res = blocker.block();
+        ASSERT(res == Blocker::Result::UNBLOCKED);
+    }
+}
+
+void SharedMutex::shared_unlock()
+{
+    LOCK_GUARD(m_state_access_lock);
+
+    ASSERT(m_shared_refcount > free_mode);
+
+    auto new_refcount = --m_shared_refcount;
+
+    if (new_refcount == free_mode && !m_exclusive_waiters.empty())
+        m_exclusive_waiters.pop_front().unblock();
+}
+
 }
--- Kernel/Multitasking/Mutex.h
+++ Kernel/Multitasking/Mutex.h
@@ -19,6 +19,29 @@ private:
     List<MutexBlocker> m_waiters;
 };
 
+class SharedMutex {
+public:
+    void exclusive_lock();
+    void exclusive_unlock();
+
+    void shared_lock();
+    void shared_unlock();
+
+private:
+    InterruptSafeSpinLock m_state_access_lock;
+
+    static constexpr ssize_t exclusive_mode = -1;
+    static constexpr ssize_t free_mode = 0;
+
+    // 0 - not owned by anyone
+    // < 0 - exclusive ownership
+    // > 0 - shared ownership of N
+    ssize_t m_shared_refcount { 0 };
+
+    List<MutexBlocker> m_shared_waiters;
+    List<MutexBlocker> m_exclusive_waiters;
+};
+
 template <>
 class LockGuard<Mutex> {
 public:
