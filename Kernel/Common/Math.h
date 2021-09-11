6e5dbe306870b9a3fb90de8b4644e07041fd052f
8infy <99danilt@gmail.com>
2021-09-11 23:20:25 +0300

    [Kernel/Common] Added abs()

--- Kernel/Common/Math.h
+++ Kernel/Common/Math.h
@@ -9,4 +9,11 @@ enable_if_t<is_integral_v<T>, T> ceiling_divide(T l, T r)
 {
     return !!l + ((l - !!l) / r);
 }
+
+template <typename T>
+enable_if_t<is_integral_v<T>, T> abs(T val)
+{
+    return val < 0 ? -val : val;
+}
+
 }
