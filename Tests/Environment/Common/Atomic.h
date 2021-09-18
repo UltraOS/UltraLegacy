966bf927a68ed622ff24eb5a602223ca83233f1f
8infy <99danilt@gmail.com>
2021-09-18 15:11:44 +0300

    [Tests] Synchronize mock Atomic with real.

--- Tests/Environment/Common/Atomic.h
+++ Tests/Environment/Common/Atomic.h
@@ -2,6 +2,15 @@
 
 namespace kernel {
 
+enum class MemoryOrder : int {
+    RELAXED,
+    CONSUME,
+    ACQUIRE,
+    RELEASE,
+    ACQ_REL,
+    SEQ_CST
+};
+
 template <typename T>
 class Atomic
 {
@@ -13,17 +22,35 @@ public:
     {
     }
 
-    operator T&()
+    T load(MemoryOrder) const { return m_value; }
+    void store(T val, MemoryOrder) { m_value = val; }
+
+    bool compare_and_exchange(T* expected, T new_value)
     {
-        return m_value;
+        if (m_value == *expected) {
+            m_value = new_value;
+            return true;
+        }
+
+        *expected = m_value;
+        return false;
     }
 
-    operator const T& () const
+    T fetch_add(T val, MemoryOrder)
     {
-        return m_value;
+        auto tmp = m_value;
+        m_value += val;
+
+        return tmp;
     }
 
-    T load() { return m_value; }
+    T fetch_subtract(T val, MemoryOrder)
+    {
+        auto tmp = m_value;
+        m_value -= val;
+
+        return tmp;
+    }
 
 private:
     T m_value;
