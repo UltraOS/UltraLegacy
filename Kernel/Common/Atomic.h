fda0f7bc6dbdf6dcd040f9d45da1938f83b9fc68
8infy <99danilt@gmail.com>
2021-09-18 14:56:19 +0300

    [Kernel/Common] Remove all operators and default arguments from Atomic
    Atomic is not a class where we can afford to hide complicated things behind
    abstractions. Every atomic operation must explicitly specify memory order
    for both optimization and better understand of what's going on.
    Previously we would have SEQ_CST everywhere for no reason just because
    it's the default argument and allows to write Q&D code.

--- Kernel/Common/Atomic.h
+++ Kernel/Common/Atomic.h
@@ -27,12 +27,12 @@ public:
     {
     }
 
-    void store(T value, MemoryOrder order = MemoryOrder::SEQ_CST) volatile ALWAYS_INLINE
+    void store(T value, MemoryOrder order) volatile ALWAYS_INLINE
     {
         __atomic_store_n(&m_value, value, static_cast<order_t>(order));
     }
 
-    T load(MemoryOrder order = MemoryOrder::SEQ_CST) const volatile ALWAYS_INLINE
+    T load(MemoryOrder order) const volatile ALWAYS_INLINE
     {
         return __atomic_load_n(&m_value, static_cast<order_t>(order));
     }
@@ -48,45 +48,14 @@ public:
             static_cast<order_t>(MemoryOrder::ACQUIRE));
     }
 
-    T exchange(T new_value) volatile ALWAYS_INLINE
-    {
-        return __atomic_exchange_n(&m_value, new_value, static_cast<order_t>(MemoryOrder::ACQUIRE));
-    }
-
-    T fetch_add(T value, MemoryOrder order = MemoryOrder::SEQ_CST) volatile ALWAYS_INLINE
+    T fetch_add(T value, MemoryOrder order) volatile ALWAYS_INLINE
     {
         return __atomic_fetch_add(&m_value, value, static_cast<order_t>(order));
     }
 
-    T fetch_subtract(T value, MemoryOrder order = MemoryOrder::SEQ_CST) volatile ALWAYS_INLINE
+    T fetch_subtract(T value, MemoryOrder order) volatile ALWAYS_INLINE
     {
         return __atomic_fetch_sub(&m_value, value, static_cast<order_t>(order));
-    }
-
-    T operator+=(T value) ALWAYS_INLINE { return fetch_add(value) + value; };
-    T operator-=(T value) ALWAYS_INLINE { return fetch_subtract(value) - value; };
-
-    T operator++() ALWAYS_INLINE { return fetch_add(1) + 1; }
-
-    T operator++(int) ALWAYS_INLINE { return fetch_add(1); }
-
-    T operator--() ALWAYS_INLINE { return fetch_subtract(1) - 1; }
-
-    T operator--(int) ALWAYS_INLINE { return fetch_subtract(1); }
-
-    operator T() const volatile ALWAYS_INLINE { return load(); }
-
-    T operator=(T value) volatile ALWAYS_INLINE
-    {
-        store(value);
-        return value;
-    }
-
-    Atomic& operator=(const Atomic& other) ALWAYS_INLINE
-    {
-        store(other.load());
-
-        return *this;
     }
 
 private:
